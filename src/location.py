c24c94cecdb01a2e8ca0f97170135a532a458cd9
Mike Gleen <mike.gleen@gmail.com>
2019-10-23 08:46:54 +0100

    checkpoint

--- src/location.py
+++ src/location.py
@@ -32,8 +32,8 @@ def loadlocs():
 
 
 def handle_check(idnum, elem):
-    if idnum not in newlocs:
-        if _args.all:
+    if _args.all:
+        if idnum not in newlocs:
             trace(3, 'Not in CSV file: {}', idnum)
         return
     objlocs = elem.findall('./ObjectLocation')
@@ -76,7 +76,7 @@ def update_one_location(ol, idnum):
         text = None
     newtext = newlocs[idnum]  # we've already checked that it's there
     if text != newtext:
-        trace(2, 'Updated {}: {} -> {}', idnum, text, newtext)
+        trace(2, '{}: Updated {} -> {}', idnum, text, newtext)
         location.text = newtext
         datebegin = ol.find('./Date/DateBegin')
         if datebegin is not None:  # normal location doesn't have a date
@@ -93,49 +93,54 @@ def handle_update(idnum, elem):
     updated = False
     if idnum in newlocs:
         objlocs = elem.findall('./ObjectLocation')
+        gotloc = False
         for ol in objlocs:
             loc = ol.get('elementtype')
             if (_args.normal and loc == 'normal location') or (
                     _args.current and loc == 'current location'
             ):
+                gotloc = True
                 updated = update_one_location(ol, idnum)
-                break
+        if not gotloc:  # cannot find correct elementtype in XML
+            trace(1, '{}: Specified object location missing', idnum)
         del newlocs[idnum]
     else:
         if _args.all:
-            trace(3, 'Not in CSV file: {}', idnum)
+            trace(3, '{}: Not in CSV file', idnum)
     if updated or _args.all:
         outfile.write(ET.tostring(elem, encoding='us-ascii'))
         total_written += 1
 
 
 def main():
-    outfile.write(b'<?xml version="1.0"?><Interchange>\n')
+    outfile.write(b'<?xml version="1.0" encoding="utf-8"?><Interchange>\n')
     for event, elem in ET.iterparse(infile):
         if elem.tag != 'Object':
             continue
         idelem = elem.find('./ObjectIdentity/Number')
         idnum = idelem.text if idelem is not None else None
         trace(3, 'idnum: {}', idnum)
-        _args.func(idnum, elem)
+        _args.func(idnum, elem)  # handle_check() or handle_update()
     outfile.write(b'</Interchange>')
     for idnum in newlocs:
-        trace(1, 'In CSV but not XML: {}', idnum)
+        trace(1, '{}: In CSV but not XML', idnum)
 
 
 def add_arguments(parser):
     parser.add_argument('infile', help='''
         The XML file saved from Modes.''')
-    parser.add_argument('outfile', help='''
-        The output XML file.''')
+    if is_update:
+        parser.add_argument('outfile', help='''
+            The output XML file.''')
     parser.add_argument('-a', '--all', action='store_true', help='''
         Write all objects and issue a warning if an object is not in the detail
         CSV file. The default is to only write updated objects. In either case
         warn if an object in the CSV file is not in the input XML file.
         ''')
-    parser.add_argument('-b', '--both', action='store_true', help='''
-        Update both the current location and the normal location.
-        Select one of "b", "n", and "c".''')
+    if is_update:
+        parser.add_argument('-b', '--both', action='store_true', help='''
+            Update both the current location and the normal location.
+            Select one of "b", "n", and "c".''')
     parser.add_argument('--col', type=int, default=1, help='''
         Specify the column in the CSV file containing the location.
         Default is 1 (first column is zero).
@@ -143,18 +148,33 @@ def add_arguments(parser):
     parser.add_argument('-c', '--current', action='store_true', help='''
         Update the current location.
         Select one of "b", "n", and "c".''')
-    parser.add_argument('-d', '--date', default=today, help='''
-        Use this string as the date to store in the new ObjectLocation date.
-        The default is today's date in Modes format (d.m.y).
+    if is_update:
+        parser.add_argument('-d', '--date', default=today, help='''
+            Use this string as the date to store in the new ObjectLocation
+            date. The default is today's date in Modes format (d.m.y).
+            ''')
+    parser.add_argument('--encoding', default='utf-8', help='''
+        Set the input encoding. Default is utf-8. Output is always ascii.
         ''')
     parser.add_argument('-n', '--normal', action='store_true', help='''
         Update the normal location.
         Select one of "b", "n", and "c".''')
+    if is_check:
+        parser.add_argument('--old', action='store_true', help='''
+            The column selected is the "old" location, the one we are moving
+            the object from. Warn if the value in the CSV file does not match
+            the value in the XML file. The default is to warn if the value in
+            the CSV file does match the value in the XML file which is not
+            expected as the purpose is to update that value.
+            You must select either --normal or --current, not --both.''')
     parser.add_argument('-v', '--verbose', type=int, default=1, help='''
         Set the verbosity. The default is 1 which prints summary information.
         ''')
     parser.add_argument('-m', '--mapfile', required=True, help='''
-        The CSV file mapping the object number to its location''')
+        The CSV file mapping the object number to its location. The object ID
+        is in the first column (column 0). The new location is by default
+        in the second column (column 1) but can be changed by the --col option.
+        ''')
 
 
 def getargs():
@@ -166,8 +186,13 @@ def getargs():
         --date option is specified.
         ''')
     subparsers = parser.add_subparsers(dest='subp')
-    check_parser = subparsers.add_parser('check')
-    update_parser = subparsers.add_parser('update')
+    check_parser = subparsers.add_parser('check', description='''
+    With no options, check that the location in the object specified by --col
+    is the same as the location specified by -c or -n option in the XML file.
+    ''')
+    update_parser = subparsers.add_parser('update', description='''
+    Update the XML file from the location in the CSV file specified by -m.
+    ''')
     check_parser.set_defaults(func=handle_check)
     update_parser.set_defaults(func=handle_update)
     add_arguments(check_parser)
@@ -182,14 +207,16 @@ def getargs():
 
 if __name__ == '__main__':
     assert sys.version_info >= (3, 6)
+    is_update = sys.argv[1] == 'update'
+    is_check = sys.argv[1] == 'check'
     today = normalize_date.modesdate(date.today())  # '[d]d.[m]m.yyyy'
     _args = getargs()
-    infile = open(_args.infile)
-    outfile = open(_args.outfile, 'wb')
+    infile = open(_args.infile, encoding=_args.encoding)
     newlocs = loadlocs()
     total_updated = 0
     total_written = 0
-    is_update = _args.subp == 'update'
+    if is_update:
+        outfile = open(_args.outfile, 'wb')
     main()
     if is_update:
         print(f'Total Updated: {total_updated}, Total Written: {total_written}')
--- src/sort_xml.py
+++ src/sort_xml.py
@@ -4,6 +4,7 @@
 Create output file sorted by object ID.
 """
 
+import argparse
 import sys
 # noinspection PyPep8Naming
 import xml.etree.ElementTree as ET  # PEP8 doesn't like two uppercase chars
@@ -13,7 +14,7 @@ from utl.normalize_obj_id import normalize
 
 def main():
     objdict = {}
-    outfile.write(b'<?xml version="1.0"?><Interchange>\n')
+    outfile.write(b'<?xml version="1.0" encoding="utf-8"?><Interchange>\n')
     seq = 0
     for event, elem in ET.iterparse(infile):
         if elem.tag != 'Object':
@@ -22,7 +23,7 @@ def main():
         num = elem.find('./ObjectIdentity/Number').text
         num = normalize(num)
         if num in objdict:
-            print (f'seq {seq}, ID {num} is a duplicate, ignored.')
+            print(f'seq {seq}, ID {num} is a duplicate, ignored.')
             continue
         objdict[num] = ET.tostring(elem, encoding='us-ascii').strip()
     for num in sorted(objdict):
@@ -31,9 +32,24 @@ def main():
     outfile.write(b'</Interchange>')
 
 
+def getargs():
+    parser = argparse.ArgumentParser(description='''
+        Create an output file sorted by Object ID.
+        ''')
+    parser.add_argument('infile', help='''
+        The XML file saved from Modes.''')
+    parser.add_argument('outfile', help='''
+        The sorted XML file.''')
+    parser.add_argument('--encoding', default='utf-8', help='''
+        Set the input encoding. Default is utf-8. Output is always ascii.
+        ''')
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == '__main__':
-    if sys.version_info.major < 3 or sys.version_info.minor < 6:
-        raise ImportError('requires Python 3.6')
-    infile = open(sys.argv[1])
-    outfile = open(sys.argv[2], 'wb')
+    assert sys.version_info >= (3, 7)
+    _args = getargs()
+    infile = open(_args.infile, encoding=_args.encoding)
+    outfile = open(_args.outfile, 'wb')
     main()
