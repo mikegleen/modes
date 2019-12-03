1b6e885c4a9da67b266f2f39f368c2f3d6cd2dd1
Mike Gleen <mike.gleen@gmail.com>
2019-12-03 14:58:52 +0000

    Working.

--- src/location.py
+++ src/location.py
@@ -10,11 +10,17 @@ from datetime import date
 import sys
 # noinspection PyPep8Naming
 import xml.etree.ElementTree as ET
-from utl import normalize_date
+import utl.normalize_date as nd
+
+NORMAL_LOCATION = 'normal location'
+CURRENT_LOCATION = 'current location'
+ELEMENTTYPE = 'elementtype'
+
+verbose = 1  # overwritten by _args.verbose; set here for unittest
 
 
 def trace(level, template, *args):
-    if _args.verbose >= level:
+    if verbose >= level:
         print(template.format(*args))
 
 
@@ -24,10 +30,21 @@ def loadlocs():
     :return: the dictionary containing the mappings
     """
     location_dict = {}
+    loc_arg = _args.location
+    need_heading = True if _args.heading else False
     with codecs.open(_args.mapfile, 'r', 'utf-8-sig') as mapfile:
         reader = csv.reader(mapfile)
         for row in reader:
-            location_dict[row[0].strip()] = row[_args.col].strip()
+            if need_heading:
+                # if --location is given just skip the first row
+                if not loc_arg and (row[_args.col].strip().lower()
+                                    != _args.heading.lower()):
+                    print(f'Failed heading check. {row[_args.col].lower()} is not '
+                          f'{_args.heading.lower()}')
+                    sys.exit(1)
+                need_heading = False
+                continue
+            location_dict[row[0].strip()] = loc_arg if loc_arg else row[_args.col].strip()
     return location_dict
 
 
@@ -38,9 +55,9 @@ def handle_check(idnum, elem):
             return
     objlocs = elem.findall('./ObjectLocation')
     for ol in objlocs:
-        loc = ol.get('elementtype')
-        if (_args.normal and loc == 'normal location') or (
-                _args.current and loc == 'current location'
+        loc = ol.get(ELEMENTTYPE)
+        if (_args.normal and loc == NORMAL_LOCATION) or (
+                _args.current and loc == CURRENT_LOCATION
         ):
             location = ol.find('./Location')
             if location.text is not None:
@@ -56,18 +73,81 @@ def handle_check(idnum, elem):
     del newlocs[idnum]
 
 
-def update_one_location(ol, idnum):
+def validate_locations(idnum, elem):
     """
-    If the location in the newlocs dictionary is different from the location
-    in the XML, update the XML, insert the date specified on the command line,
-    and delete the idnum from the global "newlocs" dictionary.
-    Note that we have already tested that idnum is in newlocs.
+    :param idnum:
+    :param elem:
+    :return: True if valid, False otherwise
+    """
+    valid = True
+    numnormal = numcurrent = 0
+    objlocs = elem.findall('./ObjectLocation')
+    for ol in objlocs:
+        loc = ol.get(ELEMENTTYPE)
+        if loc == NORMAL_LOCATION:
+            numnormal += 1
+        elif loc == CURRENT_LOCATION:
+            numcurrent += 1
+        else:
+            trace(1, '{}: Unexpected ObjectLocation elementtype = "{}"', idnum, loc)
+            return False
+    if numnormal != 1:
+        trace(1, '{}: Expected one normal location, got {}', idnum, numnormal)
+        return False
+    if numcurrent < 1:
+        trace(1, '{}: Expected one or more current locations, got 0', idnum)
+        valid = False
+    if not valid:
+        return False
 
+    # Confirm the first ObjectLocation is a normal location.
+    elttype = objlocs[0].get(ELEMENTTYPE)
+    if elttype != NORMAL_LOCATION:
+        trace(1, '{}: The first ObjectLocation is not the normal location', idnum)
+        return False
+
+    # We now know that all remaining ObjectLocation elements are current date.
+    # The first current location must have a DateBegin but no DateEnd.
+    try:
+        datebegin = objlocs[1].find('./Date/DateBegin').text
+        datebegin = nd.date(datebegin)
+    except (AttributeError, ValueError, TypeError) as err:
+        trace(1, '{}: Bad DateBegin - {}', idnum, err)
+        return False
+    dateend = objlocs[1].find('./Date/DateEnd')
+    if dateend is not None:
+        trace(1, '{}: DateEnd present in first current location; should not be', idnum)
+        return False
+
+    # All remaining current location elements must be increasingly old and must have
+    # DateEnd elements.
+    previous_datebegin = datebegin
+    for ol in objlocs[2:]:
+        try:
+            modesdatebegin = ol.find('./Date/DateBegin').text
+            datebegin = nd.date(modesdatebegin)
+        except (AttributeError, ValueError, TypeError) as err:
+            # get might return None which barfs nd.date or bad format date
+            trace(1, '{}: Bad or missing DateBegin - {}', idnum, err)
+            return False
+        if datebegin >= previous_datebegin:
+            trace(1, '{}: Dates out of order. {} >= {}', idnum, datebegin,
+                  previous_datebegin)
+            return False
+        dateend = ol.find('./Date/DateEnd')
+        if dateend is None:
+            trace(1, '{}: DateEnd missing. DateBegin: {}', idnum, modesdatebegin)
+            return False
+        previous_datebegin = datebegin
+    return valid
+
+
+def update_normal_location(ol, idnum):
+    """
     :param ol: the ObjectLocation element
-    :param idnum: the ObjectIdentity/Number text
+    :param idnum: the ObjectIdentity/Number text (we've tested that idnum is in newlocs)
     :return: True if the object is updated, False otherwise
     """
-    global total_updated
     updated = False
     location = ol.find('./Location')
     if location.text is not None:
@@ -77,37 +157,103 @@ def update_one_location(ol, idnum):
 
     newtext = _args.location if _args.location else newlocs[idnum]
     if text != newtext:
-        trace(2, '{}: Updated {} -> {}', idnum, text, newtext)
+        trace(2, '{}: Updated normal location {} -> {}', idnum, text, newtext)
         location.text = newtext
-        datebegin = ol.find('./Date/DateBegin')
-        if datebegin is not None:  # normal location doesn't have a date
-            datebegin.text = _args.date
         updated = True
-        total_updated += 1
     else:
-        trace(2, 'Unchanged: {}: {}', idnum, text)
+        trace(2, 'Normal location unchanged: {}: {}', idnum, text)
     return updated
 
 
+def update_current_location(elem, idnum):
+    """
+
+    :param elem: the Object element
+    :param idnum: the ObjectIdentity/Number text (we've tested that idnum is in newlocs)
+    :return: True if the object is updated, False otherwise
+
+    Insert a new current location element. If --reset_current is set, delete all of the
+    existing current location elements.
+
+    The order of the ObjectLocation elements is that the first one in the document is
+    the normal location followed by the current location elements with the newest ones
+    first.
+
+    """
+    subelts = list(elem)
+    normlocix = 0
+    objlocfound = False
+    for elt in subelts:
+        normlocix += 1
+        if elt.tag == 'ObjectLocation':
+            objlocfound = True
+            break
+    if not objlocfound:
+        trace(1, '{}: Cannot find ObjectLocation', idnum)
+        return False
+    # normlocix is the index of the normal location (first index is 1). This is where we
+    # will insert the new current location element.
+
+    # Update the existing current location to have a DateEnd element
+    currentlocelem = elem.find('./ObjectLocation[2]')
+    locationelt = currentlocelem.find('./Location')
+    if locationelt.text is not None:
+        text = locationelt.text.strip().upper()
+    else:
+        text = None
+    newlocation = _args.location if _args.location else newlocs[idnum]
+    if text == newlocation.upper():
+        trace(2, 'Unchanged: {}: {}', idnum, text)
+        return False
+    oldate = currentlocelem.find('./Date')
+    ET.SubElement(oldate, 'DateEnd').text = _args.date
+
+    # Insert a new ObjectLocation after the normal location
+    newloc = ET.Element('ObjectLocation')
+    newloc.set(ELEMENTTYPE, CURRENT_LOCATION)
+    ET.SubElement(newloc, 'Location').text = newlocation
+    locdate = ET.SubElement(newloc, 'Date')
+    ET.SubElement(locdate, 'DateBegin').text = _args.date
+    ET.SubElement(newloc, 'Reason').text = _args.reason
+    elem.insert(normlocix, newloc)  # insert the new element after the normal location
+    trace(2, '{}: Updated {} -> {}', idnum, text, newlocation)
+    return True
+
+
 def handle_update(idnum, elem):
-    global total_written
+    """
+    If the location in the newlocs dictionary is different from the location
+    in the XML, update the XML, insert the date specified on the command line,
+    and delete the idnum from the global "newlocs" dictionary.
+
+    :param idnum:
+    :param elem:
+    :return: None
+    """
+    global total_updated, total_written
     updated = False
     if idnum in newlocs:
-        objlocs = elem.findall('./ObjectLocation')
-        gotloc = False
-        for ol in objlocs:
-            loc = ol.get('elementtype')
-            if (_args.normal and loc == 'normal location') or (
-                    _args.current and loc == 'current location'
-            ):
-                gotloc = True
-                updated = update_one_location(ol, idnum)
-        if not gotloc:  # cannot find correct elementtype in XML
-            trace(1, '{}: Specified object location missing', idnum)
+        if not validate_locations(idnum, elem):
+            trace(1, 'Failed pre-update validation.')
+            sys.exit(1)
+        if _args.normal:
+            objlocs = elem.findall('./ObjectLocation')
+            for ol in objlocs:
+                loc = ol.get('elementtype')
+                if loc == NORMAL_LOCATION:
+                    updated = update_normal_location(ol, idnum)
+                    break
+        if _args.current:
+            updated = update_current_location(elem, idnum)
         del newlocs[idnum]
     else:
         if _args.warn:
             trace(1, '{}: Not in CSV file', idnum)
+    if idnum in newlocs and not validate_locations(idnum, elem):
+        trace(1, 'Failed post-update validation.')
+        sys.exit(1)
+    if updated:
+        total_updated += 1
     if updated or _args.all:
         outfile.write(ET.tostring(elem, encoding='us-ascii'))
         total_written += 1
@@ -122,9 +268,12 @@ def main():
         idnum = idelem.text if idelem is not None else None
         trace(3, 'idnum: {}', idnum)
         _args.func(idnum, elem)  # handle_check() or handle_update()
+        if _args.short:
+            break
     outfile.write(b'</Interchange>')
-    for idnum in newlocs:
-        trace(1, '{}: In CSV but not XML', idnum)
+    if not _args.short:  # Skip warning if only processing one object.
+        for idnum in newlocs:
+            trace(1, '{}: In CSV but not XML', idnum)
 
 
 def add_arguments(parser):
@@ -150,12 +299,16 @@ def add_arguments(parser):
         Update the current location.
         Select one of "b", "n", and "c".''')
     if is_update:
-        parser.add_argument('-d', '--date', default=today, help='''
+        parser.add_argument('-d', '--date', default=nd.modesdate(date.today()), help='''
             Use this string as the date to store in the new ObjectLocation
-            date. The default is today's date in Modes format (d.m.y).
+            date. The default is today's date in Modes format (d.m.yyyy).
             ''')
     parser.add_argument('--encoding', default='utf-8', help='''
         Set the input encoding. Default is utf-8. Output is always ascii.
+        ''')
+    parser.add_argument('--heading', help='''
+        The first row of the map file contains a heading which must match the parameter
+        (case insensitive).
         ''')
     parser.add_argument('-l', '--location', help='''
         Set the location for all of the objects. In this case the CSV file only needs a
@@ -176,7 +329,16 @@ def add_arguments(parser):
             the value in the XML file. The default is to warn if the value in
             the CSV file does match the value in the XML file which is not
             expected as the purpose is to update that value.
-            You must select either --normal or --current, not --both.''')
+            ''')
+    if is_update:
+        parser.add_argument('--reset_current', action='store_true', help='''
+        Only output the most recent current location element for each object.
+        ''')
+    parser.add_argument('-r', '--reason', default='', help='''
+        Insert this text as the reason for the move to the new current location.
+        ''')
+    parser.add_argument('-s', '--short', action='store_true', help='''
+        Only process a single object. For debugging.''')
     parser.add_argument('-v', '--verbose', type=int, default=1, help='''
         Set the verbosity. The default is 1 which prints summary information.
         ''')
@@ -210,9 +372,10 @@ def getargs():
         raise ValueError('Select one of "b", "n", and "c".')
     args.current |= args.both
     args.normal |= args.both
-    # Set a fake value for the new location as it will be taken from the --location value.
+    # Set a fake value for the new location column as it will be taken from the --location
+    # value instead of a column in the CSV file.
     if args.location:
-        args.col = 0
+        args.col = None
     return args
 
 
@@ -220,14 +383,18 @@ if __name__ == '__main__':
     assert sys.version_info >= (3, 6)
     is_update = sys.argv[1] == 'update'
     is_check = sys.argv[1] == 'check'
-    today = normalize_date.modesdate(date.today())  # '[d]d.[m]m.yyyy'
     _args = getargs()
+    verbose = _args.verbose
     infile = open(_args.infile, encoding=_args.encoding)
     newlocs = loadlocs()
+    total_in_csvfile = len(newlocs)
     total_updated = 0
     total_written = 0
     if is_update:
         outfile = open(_args.outfile, 'wb')
     main()
     if is_update:
-        print(f'Total Updated: {total_updated}, Total Written: {total_written}')
+        print(f'Total Updated: {total_updated}/{total_in_csvfile}\n'
+              f'Total Written: {total_written}')
+    if verbose >= 1:
+        print(f'End location {sys.argv[1]}.')
