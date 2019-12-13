d642d6a3981582f4f1315d33a055edb4183c6eeb
Mike Gleen <mike.gleen@gmail.com>
2019-12-13 11:52:41 +0000

    testing

--- src/test/test_location.py
+++ src/test/test_location.py
@@ -6,7 +6,6 @@ import os.path
 import unittest
 # noinspection PyPep8Naming
 import xml.etree.ElementTree as ET
-import utl.normalize as nd
 from location import validate_locations
 
 
@@ -16,8 +15,8 @@ class TestLocation(unittest.TestCase):
     TESTLOCATION = '/Users/mlg/pyprj/hrm/modes/data/test/location/xml'
     TESTFILES = [
         ('location_test01.xml', T, '1 normal, 1 current'),
-        ('location_test02.xml', T, '1 normal, 2 current'),
-        ('location_test03.xml', T, 'dateend present in first current'),
+        ('location_test02.xml', F, '1 normal, 2 current'),
+        ('location_test03.xml', F, 'dateend present in first current'),
         ('location_test04.xml', F, 'bad  DateBegin'),
         ('location_test05.xml', F, 'missing DateBegin'),
         ('location_test06.xml', F, 'DateEnd missing from old current'),
@@ -30,6 +29,12 @@ class TestLocation(unittest.TestCase):
         ('location_test13.xml', F, 'current location with no DateEnd is not the latest.'),
         ('location_test14.xml', F, 'begin date is younger than end date.'),
         ('location_test15.xml', F, 'begin date is not equal previous end date.'),
+        ('location_test16.xml', T, 'previous location precedes current location.'),
+        ('location_test17.xml', F, 'gap between end date and previous begin date.'),
+    ]
+
+    TESTSTRICTFILES = [
+        ('location_test17.xml', T, 'gap between end date and previous begin date.'),
     ]
 
     def test_validate(self):
@@ -43,6 +48,23 @@ class TestLocation(unittest.TestCase):
                 idelem = elem.find('./ObjectIdentity/Number')
                 idnum = idelem.text if idelem is not None else None
                 result = validate_locations(idnum, elem)
+                infile.close()
+                if expected:
+                    self.assertTrue(result, msg=msg)
+                else:
+                    self.assertFalse(result, msg=msg)
+
+    def test_validate_no_strict(self):
+        print('\n')
+        for filename, expected, msg in TestLocation.TESTSTRICTFILES:
+            with self.subTest(filename=filename):
+                testfile = os.path.join(TestLocation.TESTLOCATION, filename)
+                infile = open(testfile)
+                tree = ET.parse(infile)
+                elem = tree.find('Object')
+                idelem = elem.find('./ObjectIdentity/Number')
+                idnum = idelem.text if idelem is not None else None
+                result = validate_locations(idnum, elem, strict=False)
                 infile.close()
                 if expected:
                     self.assertTrue(result, msg=msg)
@@ -85,5 +107,4 @@ if __name__ == '__main__':
 """
 
 if __name__ == '__main__':
-     unittest.main()
-
+    unittest.main()
