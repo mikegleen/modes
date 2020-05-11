e84d8879ab3947a3ba7a3380470bbeed1953ed24
Mike Gleen <mike.gleen@gmail.com>
2020-05-11 11:01:47 +0100

    ckpt

--- test/update_from_csv/bin/runner.sh
+++ test/update_from_csv/bin/runner.sh
@@ -1,16 +1,20 @@
 ap=test/update_from_csv/bin/update_from_csv.sh
+tl=test/update_from_csv/bin/testlist.txt
 testsrun=0
 testsfailed=0
 #
 function run {
     let testsrun++
     $ap $*
-    if (( $? != 0))
+    if (( $? != 0 ))
     then
         let testsfailed++
     fi
 }
-run TEST01 --heading -v 0
+#
+while read -r; do
+    run $REPLY
+done < $tl
 #
 echo $testsrun tests run.
 echo $testsfailed tests failed.
--- test/update_from_csv/bin/update_from_csv.sh
+++ test/update_from_csv/bin/update_from_csv.sh
@@ -1,12 +1,27 @@
 # Call this script with the name of the test followed by any parameters to pass to python.
+# It assumes that the working directory, modes, has a subdirectory tree as follows:
 #
+# modes
+# ├── test
+# │   ├── bin
+# │   ├── data
+# │   └── update_from_csv
+# │       ├── baseline
+# │       ├── bin
+# │       ├── csv
+# │       ├── results
+# │       ├── xml
+# │       └── yml
+#
+# If this shell script name is ".../modes/test/xxx/bin/xxx.sh" then app wil be "xxx".
 app=`basename $0|cut -d. -f1`
 tpath=test/$app
-tst=$1
-shift
+tst=$1  # get the test name
+shift  # now $* contains the parameters to pass to the app
 python src/$app.py $tpath/xml/$tst.xml $tpath/results/$tst.xml -c $tpath/yml/$tst.yml -m  $tpath/csv/$tst.csv $*
 diff -q $tpath/baseline/$tst.xml $tpath/results/$tst.xml &>/dev/null
-if [ $? ]
+if [ $? ]  # if bad compare
 then
+# display the names of the offending files
 diff -q $tpath/baseline/$tst.xml $tpath/results/$tst.xml
 fi
