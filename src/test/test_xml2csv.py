2cf09e3f0f4967895604060d0c1bc9342855b401
Mike Gleen <mike.gleen@gmail.com>
2020-05-18 17:40:59 +0100

    Working with subtests

--- src/test/test_xml2csv.py
+++ src/test/test_xml2csv.py
@@ -6,32 +6,46 @@ import os.path
 import unittest
 # noinspection PyPep8Naming
 import xml.etree.ElementTree as ET
-from xml2csv import main
+from xml2csv import main, getargs
 
 
 class TestXml2csv(unittest.TestCase):
-    T = True
-    F = False
-    TESTLOCATION = '/Users/mlg/pyprj/hrm/modes/data/test/xml2csv'
-    TESTS = [
+    TESTLOCATION = '/Users/mlg/pyprj/hrm/modes/test/xml2csv'
+    DEFAULT_FIXEDPARAMS = ['--heading', '-v', '0']
+    TESTFILES = [
         # (test number, expected rows, expected not found, columns in 1st row
-        (1, 1, 0, 1),
+        (1, 1, 0, None),
+        (2, 1, 0, None),
+        (3, 1, 1, None),
+        (4, 1, 0, None),
+        (5, 0, 0, None),
+        (6, 2, 0, None),
     ]
+
+    def onetest(self, test_tuple):
+        (testnum, expected_rows, expected_notfound, fixedparams) = test_tuple
+        if fixedparams is None:
+            fixedparams = TestXml2csv.DEFAULT_FIXEDPARAMS
+        filename = f'test{testnum:02}'
+        xmlfilename = os.path.join(TestXml2csv.TESTLOCATION, 'xml',
+                                   filename + '.xml')
+        ymlfilename = os.path.join(TestXml2csv.TESTLOCATION, 'yml',
+                                   filename + '.yml')
+        csvfilename = os.path.join(TestXml2csv.TESTLOCATION, 'results',
+                                   filename + '.csv')
+        params = (['dummy_prog_name', xmlfilename, csvfilename, '-c',
+                   ymlfilename] + fixedparams)
+        n_rows, n_notfound = main(params)
+        self.assertEqual(expected_rows, n_rows,
+                         f'Expected rows, actual rows in {filename}')
+        self.assertEqual(expected_notfound, n_notfound,
+                         f'Expected not found, actual not found in {filename}')
 
     def test_xml2csv(self):
         print('\n')
         for test_tuple in TestXml2csv.TESTFILES:
-            testnum, expected_rows, expected_notfound, expected_columns = (
-                test_tuple)
-            filename = f'test{testnum:02}'
             with self.subTest():
-                xmlfilename = os.path.join(TestXml2csv.TESTLOCATION, 'xml',
-                                           filename)
-                ymlfilename = os.path.join(TestXml2csv.TESTLOCATION, 'yml',
-                                           filename)
-                csvfilename = os.path.join(TestXml2csv.TESTLOCATION, 'csv',
-                                           filename)
-                n_lines, not_found = main(xmlfilename, ymlfilename, _args.outfile)
+                self.onetest(test_tuple)
 
 
 if __name__ == '__main__':
