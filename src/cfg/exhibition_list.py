8997f0b85299a37a8c2769e46c9b3a3fcf12687f
Mike Gleen <mike.gleen@gmail.com>
2021-02-17 15:45:52 +0000

    Initial commit.

---
+++ src/cfg/exhibition_list.py
@@ -0,0 +1,17 @@
+EXSTR = '''Serial,DateBegin,DateEnd,ExhibitionName
+1,15/10/2016,08/01/2017,Heath Robinson at War
+2,21/01/2017,26/03/2017,The Brothers Robinson
+3,01/04/2017,11/06/2017,Edward Ardizzone’s Illustrations
+4,17/06/2017,03/09/2017,Rejuvenated Junk
+5,09/09/2017,26/11/2017,Visualizing the Water Babies
+6,02/12/2017,18/02/2018,Heath Robinson’s World of Advertising
+7,24/02/2018,20/05/2018,Neo-Romantic Book Illustrations in Britain 1943-1955
+8,26/05/2018,19/08/2018,"A Curious Turn: Moving, Mechanical Sculpture"
+9,25/08/2018,18/11/2018,Peter Pan and Other Lost Children
+10,01/11/2018,18/11/2018,Special Mini Exhibition in Honour of WWI Centenary
+11,24/11/2018,24/02/2019,Heath Robinson’s Home Life
+12,02/03/2019,19/05/2019,The Beardsley Generation
+13,25/05/2019,01/09/2019,Tim Lewis: Post Nature
+14,07/09/2019,24/11/2019,Heath Robinson Watercolours
+15,30/11/2019,23/02/2020,Fairies in Illustration
+16,29/02/2020,20/03/2021,Charles Keeping Illustrations'''
