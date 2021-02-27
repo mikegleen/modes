a004278ce51f70249fb96343b1dd57cd5ff6eec3
Mike Gleen <mike.gleen@gmail.com>
2021-02-27 16:18:31 +0000

    bugfix, minor, refactor

--- src/exhibition.py
+++ src/exhibition.py
@@ -46,7 +46,7 @@ def one_object(objelt, idnum, exhibition: Exhibition):
     :param objelt: the Object
     :param idnum: the ObjectIdentity/Number text (for trace)
     :param exhibition: the Exhibition tuple corresponding to exhibition_list.py
-    :return: None. The exhibition is inserted into the Object element.
+    :return: a tuple containing the BeginDate, the new Exhibition element
     """
     def new_exhib():
         newelt = ET.Element('Exhibition')
@@ -59,7 +59,7 @@ def one_object(objelt, idnum, exhibition: Exhibition):
         subelt.text = modesdate(exhibition.DateBegin)
         subelt = ET.SubElement(dateelt, 'DateEnd')
         subelt.text = modesdate(exhibition.DateEnd)
-        return datefrommodes(exhibition.DateBegin), subelt
+        return exhibition.DateBegin, newelt
 
     def one_exhibition(exhib_elt):
         """
@@ -88,7 +88,7 @@ def one_object(objelt, idnum, exhibition: Exhibition):
                     if dateelt.tag == 'DateBegin':
                         dateelt.text = modesdate(exhibition.DateBegin)
                     elif dateelt.tag == 'DateEnd':
-                        dateelt.text = modesdate(exhibition.DateBegin)
+                        dateelt.text = modesdate(exhibition.DateEnd)
             else:
                 trace(1, 'ID {}: Unknown subelt in {} Exhibition element: {},' 
                       ' element not updated.', display_id, tag)
@@ -98,6 +98,8 @@ def one_object(objelt, idnum, exhibition: Exhibition):
     display_id = denormalize_id(idnum)
     trace(2, 'one_element: {} {}', display_id, exhibition)
     elts = list(objelt)  # the children of Object
+    # for elt in elts:
+    #     print(elt)
     exhibs_to_insert = list()
     exhibs_to_remove = list()
     firstexix = None
@@ -105,14 +107,14 @@ def one_object(objelt, idnum, exhibition: Exhibition):
     for n, elt in enumerate(elts):
         if elt.tag == "Exhibition":
             status = one_exhibition(elt)
-            if status:
+            if status:  # 1 or 2
                 # Assume DateBegin exists
                 begindate = datefrommodes(elt.find('./Date/DateBegin').text)
                 exhibs_to_insert.append((begindate, elt))
                 if status == 2:
                     need_new = False
             else:
-                exhibs_to_remove.append(elt)
+                exhibs_to_remove.append(elt)  # empty template
             if firstexix is None:
                 firstexix = n
     if firstexix is None:  # no Exhibition elements were found
@@ -123,14 +125,16 @@ def one_object(objelt, idnum, exhibition: Exhibition):
             if elt.tag == "Acquisition":
                 firstexix = n + 1  # insert the new elt after <Acquisition>
                 break
-    if need_new:
-        newexhibit = new_exhib()
-        exhibs_to_insert.append(newexhibit)
-    for exhib in exhibs_to_insert:
+    for _edate, exhib in exhibs_to_insert:
+        # print(objelt, exhib)
         objelt.remove(exhib)
     for exhib in exhibs_to_remove:
         objelt.remove(exhib)
-    for exhib in sorted(exhibs_to_insert, reverse=True):
+    if need_new:
+        newexhibit = new_exhib()  # returns a tuple of (date, element)
+        exhibs_to_insert.append(newexhibit)
+    # Insert the Exhibition elements so that they are in ascending date order
+    for _edate, exhib in sorted(exhibs_to_insert, reverse=True):
         objelt.insert(firstexix, exhib)
 
 
@@ -157,34 +161,37 @@ def get_exhibition_dict():
 
 def get_csv_dict(csvfile):
     """
-
     :param csvfile: contains the accession number and optionally the exhibition
-                    number in columns specified by --serial and --ex_col
-    :return: A dict with the key of the serial number and the value being
+                    number in columns specified by --col_acc and --col_ex
+    :return: A dict with the key of the accession number and the value being
              the exhibition number.
     """
     with codecs.open(csvfile, 'r', 'utf-8-sig') as mapfile:
         cdict = {}
         reader = csv.reader(mapfile)
+        for n in range(_args.skiprows):
+            next(reader)
         for row in reader:
-            serial = normalize_id(row[_args.serial])
+            accnum = normalize_id(row[_args.col_acc])
             if _args.exhibition:
                 exhibition = _args.exhibition
             else:
-                ex_col = _args.ex_col
+                col_ex = _args.col_ex
                 try:
-                    exhibition = int(row[ex_col])
+                    exhibition = int(row[col_ex])
                 except IndexError as e:
-                    print(f'Missing column {ex_col}, accession #: {serial}')
+                    print(f'Missing column {col_ex}, accession #: {accnum}')
                     raise e
-            cdict[serial] = exhibition
+            if accnum in cdict:
+                raise ValueError(f'Duplicate accession number: {accnum}')
+            cdict[accnum] = exhibition
     trace(2, 'get_csv_dict: {}', cdict)
     return cdict
 
 
 def main():
     outfile.write(b'<?xml version="1.0"?><Interchange>\n')
-    exmap = get_csv_dict(_args.mapfile)  # serial -> exhibition #
+    exmap = get_csv_dict(_args.mapfile)  # col_acc -> exhibition #
     exdict = get_exhibition_dict()  # exhibition # -> Exhibition tuple
     written = 0
     for event, elem in ET.iterparse(infile):
@@ -209,7 +216,8 @@ def main():
     outfile.write(b'</Interchange>')
     for idnum in exmap:
         trace(1, 'In CSV but not XML: "{}"', idnum)
-    trace(1, f'End exhibition.py. {written} objects written.')
+    trace(1, f'End exhibition.py. {written} object'
+             f'{"s" if written != 1 else ""} written.')
 
 
 def getargs():
@@ -217,7 +225,7 @@ def getargs():
         Import exhibition information into a Modes XML file.
         Read a CSV file containing one or two columns. The first column is
         the accession number whose record should be updated. The second
-        column is the optional exhibition number
+        column is the optional exhibition number.
         (see --exhibition for details).
         ''')
     parser.add_argument('infile', help='''
@@ -226,21 +234,21 @@ def getargs():
         The output XML file.''')
     parser.add_argument('-a', '--all', action='store_true', help='''
         Write all objects. The default is to only write updated objects.''')
-    parser.add_argument('-e', '--exhibition', required=False,
-                        type=int, help='''
-        The exhibition number, corresponding to the data in exhibition_list.py
-        to apply to all objects in the CSV file.''')
-    parser.add_argument('--ex_col', default=1, type=int, help='''
+    parser.add_argument('--col_acc', default=0, type=int, help='''
+        The zero-based column containing the accession number of the
+        object to be updated. ''')
+    parser.add_argument('--col_ex', default=1, type=int, help='''
         The zero-based column containing the exhibition number.
         Ignored if --exhibition is specified. ''')
+    parser.add_argument('-e', '--exhibition', type=int, help='''
+        The exhibition number, corresponding to the data in exhibition_list.py
+        to apply to all objects in the CSV file.''')
     parser.add_argument('-m', '--mapfile', required=True, help='''
         The CSV file mapping the object number to the exhibition number
         (but see --exhibition). There is no heading row.''')
-    parser.add_argument('--serial', required=False, default=0, type=int,
-                        help='''
-        The zero-based column number containing the accession number of the
-        object to be updated. ''')
-    parser.add_argument('-s', '--short', action='store_true', help='''
+    parser.add_argument('-s', '--skiprows', type=int, default=0, help='''
+        Number of lines to skip at the start of the CSV file''')
+    parser.add_argument('--short', action='store_true', help='''
         Only process one object. For debugging.''')
     parser.add_argument('-v', '--verbose', type=int, default=1, help='''
         Set the verbosity. The default is 1 which prints summary information.
@@ -251,10 +259,7 @@ def getargs():
 
 if __name__ == '__main__':
     assert sys.version_info >= (3, 6)
-    # print(exdict)
-    # sys.exit()
     _args = getargs()
-    nupdated = nunchanged = 0
     infile = open(_args.infile)
     outfile = open(_args.outfile, 'wb')
     trace(1, 'Input file: {}', _args.infile)
