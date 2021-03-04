985e04ccc86863435139b29f7d64a14c8bf65980
Mike Gleen <mike.gleen@gmail.com>
2021-03-04 16:06:07 +0000

    Implement the CatalogueNumber element. Handle empty accession number values. Change the order of the Exhibition elements so that the most recent is first.

--- src/exhibition.py
+++ src/exhibition.py
@@ -12,6 +12,7 @@
     The Exhibition group template is:
         <Exhibition>
             <ExhibitionName />
+            <CatalogueNumber />
             <Place />
             <Date>
                 <DateBegin />
@@ -40,18 +41,22 @@ def trace(level, template, *args):
         print(template.format(*args))
 
 
-def one_object(objelt, idnum, exhibition: Exhibition):
+def one_object(objelt, idnum, exhibition: Exhibition, catalog_num=None):
     """
 
     :param objelt: the Object
     :param idnum: the ObjectIdentity/Number text (for trace)
     :param exhibition: the Exhibition tuple corresponding to exhibition_list.py
+    :param catalog_num: for the CatalogueNumber element
     :return: a tuple containing the BeginDate, the new Exhibition element
     """
     def new_exhib():
         newelt = ET.Element('Exhibition')
         subelt = ET.SubElement(newelt, 'ExhibitionName')
         subelt.text = exhibition.ExhibitionName
+        if catalog_num is not None:
+            subelt = ET.SubElement(newelt, 'CatalogueNumber')
+            subelt.text = str(catalog_num)
         subelt = ET.SubElement(newelt, 'Place')
         subelt.text = exhibition.Place
         dateelt = ET.SubElement(newelt, 'Date')
@@ -63,9 +68,10 @@ def one_object(objelt, idnum, exhibition: Exhibition):
 
     def one_exhibition(exhib_elt):
         """
+        Handle an existing exhibition
         :param exhib_elt: an Exhibition element (under Object)
         :return: 0 if it is an empty template
-                 1 if the input element if the ExhibitionName is different from
+                 1 if the input element's ExhibitionName is different from
                     the one we are inserting
                  2 (also update the values) if the ExhibitionName values match
         """
@@ -102,7 +108,7 @@ def one_object(objelt, idnum, exhibition: Exhibition):
     #     print(elt)
     exhibs_to_insert = list()
     exhibs_to_remove = list()
-    firstexix = None
+    firstexix = None  # index of the first Exhibition element
     need_new = True
     for n, elt in enumerate(elts):
         if elt.tag == "Exhibition":
@@ -133,8 +139,8 @@ def one_object(objelt, idnum, exhibition: Exhibition):
     if need_new:
         newexhibit = new_exhib()  # returns a tuple of (date, element)
         exhibs_to_insert.append(newexhibit)
-    # Insert the Exhibition elements so that they are in ascending date order
-    for _edate, exhib in sorted(exhibs_to_insert, reverse=True):
+    # Insert the Exhibition elements with the most recent one first
+    for _edate, exhib in sorted(exhibs_to_insert):
         objelt.insert(firstexix, exhib)
 
 
@@ -156,13 +162,18 @@ def get_exhibition_dict():
                         ExhibitionName=row[3],
                         Place=row[4] if len(row) >= 5 else 'HRM'
                         ) for row in reader}
+    if _args.exhibition:
+        exnum = _args.exhibition
+        trace(1, 'Processing exhibition {}: "{}"', exnum,
+              exdic[exnum].ExhibitionName)
     return exdic
 
 
 def get_csv_dict(csvfile):
     """
-    :param csvfile: contains the accession number and optionally the exhibition
-                    number in columns specified by --col_acc and --col_ex
+    :param csvfile: contains the accession number specified by --col_acc,
+    optionally the exhibition number in the column specified by  and --col_ex,
+    and optionally the catalog number specified by --col_cat.
     :return: A dict with the key of the accession number and the value being
              the exhibition number.
     """
@@ -172,7 +183,14 @@ def get_csv_dict(csvfile):
         for n in range(_args.skiprows):
             next(reader)
         for row in reader:
-            accnum = normalize_id(row[_args.col_acc])
+            accnum = row[_args.col_acc]
+            if not accnum:
+                continue  # blank accession number
+            try:
+                accnum = normalize_id(accnum)
+            except ValueError:
+                print(f"Skipping in csv: {accnum}")
+                continue
             if _args.exhibition:
                 exhibition = _args.exhibition
             else:
@@ -184,16 +202,22 @@ def get_csv_dict(csvfile):
                     raise e
             if accnum in cdict:
                 raise ValueError(f'Duplicate accession number: {accnum}')
-            cdict[accnum] = exhibition
+            cataloguenumber = None
+            if _args.col_cat is not None:
+                cataloguenumber = row[_args.col_cat]
+            # print(row)
+            # print(exhibition, cataloguenumber)
+            cdict[accnum] = (exhibition, cataloguenumber)
     trace(2, 'get_csv_dict: {}', cdict)
     return cdict
 
 
 def main():
     outfile.write(b'<?xml version="1.0"?><Interchange>\n')
-    exmap = get_csv_dict(_args.mapfile)  # col_acc -> exhibition #
+    exmap = get_csv_dict(_args.mapfile)  # acc # -> (exhibition #, catalog #)
     exdict = get_exhibition_dict()  # exhibition # -> Exhibition tuple
     written = 0
+    numupdated = 0
     for event, elem in ET.iterparse(infile):
         if elem.tag != 'Object':
             continue
@@ -202,10 +226,11 @@ def main():
         idnum = normalize_id(idnum)
         trace(3, 'idnum: {}', idnum)
         if idnum and idnum in exmap:
-            exnum = exmap[idnum]
-            one_object(elem, idnum, exdict[exnum])
+            exnum, cataloguenumber = exmap[idnum]
+            one_object(elem, idnum, exdict[exnum], cataloguenumber)
             del exmap[idnum]
             updated = True
+            numupdated += 1
         else:
             updated = False
         if updated or _args.all:
@@ -217,7 +242,8 @@ def main():
     for idnum in exmap:
         trace(1, 'In CSV but not XML: "{}"', idnum)
     trace(1, f'End exhibition.py. {written} object'
-             f'{"s" if written != 1 else ""} written.')
+             f'{"s" if written != 1 else ""} written '
+             f'of which {numupdated} updated.')
 
 
 def getargs():
@@ -237,6 +263,9 @@ def getargs():
     parser.add_argument('--col_acc', default=0, type=int, help='''
         The zero-based column containing the accession number of the
         object to be updated. ''')
+    parser.add_argument('--col_cat', type=int, help='''
+        The zero-based column containing the catalog number of the
+        object in the corresponding exhibition. ''')
     parser.add_argument('--col_ex', default=1, type=int, help='''
         The zero-based column containing the exhibition number.
         Ignored if --exhibition is specified. ''')
