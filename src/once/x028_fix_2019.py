5858ce38e576437a163cd9ad10dd01b8024bee60
Mike Gleen <mike.gleen@gmail.com>
2021-07-26 16:59:47 +0100

    minor

--- src/once/x028_fix_2019.py
+++ src/once/x028_fix_2019.py
@@ -4,16 +4,16 @@
 import os.path
 import re
 import sys
-indir = sys.argv[1]
 
+indir = sys.argv[1]
 files = os.listdir(indir)
 
 for fn in files:
-    m = re.match(r'Brinsmead LDHRM2019_(\d+).jpg', fn)
+    m = re.match(r'Brinsmead LDHRM2019_(\d+)\.jpg', fn)
     if not m:
         print(f'Skipping: {fn}')
         continue
-    seq = int(m.group(1))
+    seq = int(m.group(1))  # remove leading zeroes
     newfn = f'LDHRM.2019.{seq}.jpg'
     src = os.path.join(indir, fn)
     dst = os.path.join(indir, newfn)
