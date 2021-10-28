07de5baadf1d8c5a6a7705af64d56bc3efa078a6
Mike Gleen <mike.gleen@gmail.com>
2021-10-28 09:37:08 +0100

    change return to list of tuples

--- src/utl/list_objects.py
+++ src/utl/list_objects.py
@@ -4,18 +4,21 @@
         <normalized id>, <not normalized id>
 """
 
+from collections import namedtuple
 import sys
 # noinspection PyPep8Naming
 import xml.etree.ElementTree as ET
 from utl.cfgutil import Config
 from utl.normalize import normalize_id
 
+Obj_id = namedtuple('Obj_id', 'normalized original')
+
 
 def list_objects(infile):
     """
     Extract the accession numbers from the Object elements in an XML file
     :param infile: filename or file object containing XML data
-    :return: a list of tuples of (<normalized id>, <not normalized id>)
+    :return: a list of Obj_id namedtuples of (normalized, display)
     """
     objectlevel = 0
     config = Config()  # get default values
@@ -37,7 +40,7 @@ def list_objects(infile):
         idelem = elem.find(config.record_id_xpath)
         idnum = idelem.text if idelem is not None else ''
         if idnum:
-            idnums.append((normalize_id(idnum), idnum))
+            idnums.append(Obj_id(normalize_id(idnum), idnum))
 
     idnums.sort()
     return idnums
