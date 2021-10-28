00da361862735bf8cbc3849ce2595ca5070edb4b
Mike Gleen <mike.gleen@gmail.com>
2021-10-28 09:34:12 +0100

    init

---
+++ bin/collection/batch01.sh
@@ -0,0 +1,17 @@
+#
+# For the files in a batch, create the CSV file to be uploaded to WordPress for the collection images.
+#
+BATCH=batch1
+MODESFILE=prod_save/normal/2021-10-13_prod-sorted.xml
+#
+# Create a CSV file with the accession numbers from the filenames in the batch
+#
+python src/dir2csv.py ../collection/webimgs/${BATCH} tmp/${BATCH}_list.csv
+#
+# Pull the relevant fields from the Modes XML file for the objects in the batch
+#
+python src/xml2csv.py $MODESFILE tmp/${BATCH}_step1.csv -c src/cfg/website.yml --include tmp/${BATCH}_list.csv --include_skip 1 --heading -v 2 -b -l results/reports/${BATCH}_website.txt
+#
+# Modify the CSV file to included new and adjusted columns.
+#
+python src/web/recode_collection.py tmp/${BATCH}_step1.csv results/csv/collection/${BATCH}.csv
