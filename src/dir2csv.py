d0f7fb4fcbfafb7fad644a6e16453b9773386813
Mike Gleen <mike.gleen@gmail.com>
2021-11-08 12:33:15 +0000

    Move code to a function.

--- src/dir2csv.py
+++ src/dir2csv.py
@@ -7,20 +7,32 @@ import os.path
 import re
 import sys
 
-if len(sys.argv) != 3:
-    print('Parameters: input folder, output csv')
-    sys.exit()
-indir = sys.argv[1]
-outfile = open(sys.argv[2], 'w')
 
-files = os.listdir(indir)
-nout = 0
-print('Serial', file=outfile)
-for fn in files:
-    m = re.match(r'(collection_)?(.+)\.jpg', fn)
-    if not m:
-        print(f'Skipping: {fn}')
-        continue
-    print(m.group(2), file=outfile)
-    nout += 1
-print(f'End dir2csv. {nout} rows written.')
+def dir2csv(jpegdir):
+    jpglist = list()
+    jpgfiles = os.listdir(jpegdir)
+    for jpgfile in jpgfiles:
+        m = re.match(r'(collection_)?(.+)\.jpg', jpgfile)
+        if not m:
+            print(f'dir2csv skipping: {jpgfile}')
+            continue
+        jpglist.append(m.group(2))
+    return jpglist
+
+
+if __name__ == '__main__':
+    assert sys.version_info >= (3, 9)
+    if len(sys.argv) != 3:
+        print('Parameters: input folder, output csv')
+        sys.exit()
+    indir = sys.argv[1]
+    outfile = open(sys.argv[2], 'w')
+
+    files = os.listdir(indir)
+    outlist = dir2csv(indir)
+    nout = 0
+    print('Serial', file=outfile)
+    for fn in outlist:
+        print(fn, file=outfile)
+        nout += 1
+    print(f'End dir2csv. {nout} rows written.')
