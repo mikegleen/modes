717128eab6e705b69b4bd0e76d676813422ee362
Mike Gleen <mike.gleen@gmail.com>
2021-12-12 15:03:59 +0000

    minor

--- bin/exhibition/willard.sh
+++ bin/exhibition/willard.sh
@@ -1,1 +1,1 @@
-python src/exhibition.py prod_save/normal/2021-12-01_prod_save.xml prod_update/normal/2021-03-07_exhibition.xml -e 25 -m results/csv/exhibitions/basingstoke_r1.csv -s 1 --col_acc 1 -a
+python src/exhibition.py prod_save/normal/2021-12-01_prod_save.xml prod_update/normal/2021-12-09_exhibition.xml -e 25 -m results/csv/exhibitions/basingstoke_r1.csv -s 1 --col_acc 1 -a --col_cat 6
