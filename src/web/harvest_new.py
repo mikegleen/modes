2542bf9effd38194d0fd73f68cb497e9ebbf452b
Mike Gleen <mike.gleen@gmail.com>
2022-01-15 18:11:45 +0000

    cosmetic

--- src/web/harvest_new.py
+++ src/web/harvest_new.py
@@ -17,7 +17,7 @@ def trace(level, template, *args):
 def getargs():
     parser = argparse.ArgumentParser(description='''
     For each file in a "candidate" folder, if that file is not in the "done"
-    folder, copy it to a "harvested" folder.
+    folder or any of its subfolders, copy it to a "harvested" folder.
         ''')
     parser.add_argument('-c', '--candidate', required=True, help='''
         Directory containing new files that may need to be transferred''')
@@ -28,11 +28,13 @@ def getargs():
         Directory to contain files to be transferred
         ''')
     parser.add_argument('--dryrun', action='store_true', help='''
-        Do not copy files. Just print info.''')
+        Do not copy files. Just print info. Implies --verbose 2.''')
     parser.add_argument('-v', '--verbose', type=int, default=1, help='''
         Set the verbosity. The default is 1 which prints summary information.
         ''')
     args = parser.parse_args()
+    if args.dryrun:
+        args.verbose = 2
     return args
 
 
@@ -64,7 +66,7 @@ if __name__ == '__main__':
             frompath = os.path.join(candidatedir, candidate)
             topath = os.path.join(stagingdir, candidate)
             trace(1, 'harvesting: {}', candidate)
-            trace(2, 'from {}, to {}', frompath, topath)
+            trace(2, '    from {}, to {}', frompath, topath)
             if not _args.dryrun:
                 shutil.copy(frompath, topath)
     trace(1, '{} copied of {} candidates', ncopied, ncandidates)
