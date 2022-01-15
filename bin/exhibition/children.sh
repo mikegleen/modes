61751e4624ceb111d2845148a0540d339ed62454
Mike Gleen <mike.gleen@gmail.com>
2022-01-15 18:13:33 +0000

    init

---
+++ bin/exhibition/children.sh
@@ -0,0 +1,2 @@
+ python src/exhibition.py prod_update/normal/2022-01-06_accuracy.xml prod_update/normal/2022-01-15_exhibition.xml -e 24 -m results/csv/exhibitions/children_3.csv -s 1 --col_acc 1 --allow_missing --col_cat 6 -a
+
