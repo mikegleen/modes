2cd3146a68f68b4822368f76fea04b3ca5f66064
Mike Gleen <mike.gleen@gmail.com>
2022-02-03 22:10:03 +0000

    init

---
+++ bin/update/willis_loc_fix.sh
@@ -0,0 +1,2 @@
+python src/location.py update -i prod_update/pretty/2022-01-30_children_pretty.xml -o prod_update/pretty/2022-02-01_willis_u_pretty.xml -c -d 27.10.2021 -l 'Willis Museum' -m /Users/mlg/pyprj/hrm/modes/data/csv/update_willis.csv -a
+
