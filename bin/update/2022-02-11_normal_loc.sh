507e37da6fd7364b740b0849bad3e61e15f69912
Mike Gleen <mike.gleen@gmail.com>
2022-02-12 18:09:48 +0000

    minor

--- bin/update/2022-02-11_normal_loc.sh
+++ bin/update/2022-02-11_normal_loc.sh
@@ -1,2 +1,1 @@
- python src/location.py update -i prod_update/normal/2022-02-01_willis_u.xml -o prod_update/normal/2022-02-11_normal_loc.xml -a -n -m data/csv/2022-02-11_normal_loc.csv -v 2                                                          10:25:21
-
+ python src/location.py update -i prod_update/normal/2022-02-01_willis_u.xml -o prod_update/normal/2022-02-11_normal_loc.xml -a -n -m data/csv/2022-02-11_normal_loc.csv -v 2
