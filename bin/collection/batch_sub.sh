88c02b8713370d6677cbbb0adab0f8b6a9510e7e
Mike Gleen <mike.gleen@gmail.com>
2022-04-03 14:59:50 +0100

    minor

--- bin/collection/batch_sub.sh
+++ bin/collection/batch_sub.sh
@@ -1,21 +1,22 @@
+#!/bin/zsh
 #
 # Create a CSV file with the accession numbers from the filenames in the batch.
 # Insert a heading in the CSV file even though it will be skipped by xml2csv (useful for debugging).
 #
 set -e
-if [ -z "$REVISION" ];
-then
-	export BR=${BATCH}
+if [ -z "$REVISION" ]; then
+	BR=${BATCH}
 else
-	export BR=${BATCH}.${REVISION}
+	BR=${BATCH}.${REVISION}
 fi
+DESTDIR=../collection/etc/$BATCH
+mkdir -p $DESTDIR
 python src/dir2csv.py ../collection/aawebimgs/${BATCH} tmp/${BR}_list.csv --heading
 #
 # Pull the relevant fields from the Modes XML file for the objects in the batch.
 #
-python src/xml2csv.py $MODESFILE tmp/${BR}_step1.csv -c src/cfg/website.yml --include tmp/${BR}_list.csv --include_skip 1 --heading -v 1 -b -l results/reports/${BR}_website.log
+python src/xml2csv.py $MODESFILE $DESTDIR/${BR}_step1.csv -c src/cfg/website.yml --include tmp/${BR}_list.csv --include_skip 1 --heading -v 1 -b -l results/reports/${BR}_website.log
 #
 # Modify the CSV file to included new and adjusted columns.
 #
-mkdir -p ../collection/etc/$BATCH
-python src/web/recode_collection.py tmp/${BR}_step1.csv ../collection/etc/${BATCH}/${BR}.csv
+python src/web/recode_collection.py $DESTDIR/${BR}_step1.csv $DESTDIR/${BR}.csv
