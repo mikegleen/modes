198053965796749689fec6a594b3b1fd4efad0c2
Mike Gleen <mike.gleen@gmail.com>
2022-04-30 07:41:55 +0100

    cosmetic

--- src/play/exif.py
+++ src/play/exif.py
@@ -1,16 +1,18 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
+import sys
 
-iname = '/Volumes/Transcend/Downloads/hrmalt/accessioned_medium_res/JB006.jpg'
+# iname = '/Volumes/Transcend/Downloads/hrmalt/accessioned_medium_res/JB006.jpg'
+iname = sys.argv[1]
 img = Image.open(iname)
-exifdata=img.getexif()
+exifdata = img.getexif()
 
 for tag_id in exifdata:
-     # get the tag name, instead of human unreadable tag id
-     tag = TAGS.get(tag_id, tag_id)
-     data = exifdata.get(tag_id)
-     # decode bytes
-     if isinstance(data, bytes):
-         data = data.decode()
-     print(f"{tag_id:04X},{tag:25}: {data}")
-
+    #
+    # get the tag name, instead of human unreadable tag id
+    tag = TAGS.get(tag_id, tag_id)
+    data = exifdata.get(tag_id)
+    # decode bytes
+    if isinstance(data, bytes):
+        data = data.decode()
+    print(f"{tag_id:04X},{tag:25}: {data}")
