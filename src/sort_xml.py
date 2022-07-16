b6e09b40ab42e15ccc8d5a68fd46085c0aede8e2
Mike Gleen <mike.gleen@gmail.com>
2022-07-16 10:45:47 -0400

    Make the output file optional. Report duplicate accession numbers.

--- src/sort_xml.py
+++ src/sort_xml.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 
-Create output file sorted by object ID.
+Create output file sorted by object ID if the output file is specified.
+
+Report and discard any records with duplicate accession numbers.
 """
 
 import argparse
@@ -14,20 +16,25 @@ from utl.normalize import normalize_id, DEFAULT_MDA_CODE
 
 def main():
     objdict = {}
-    outfile.write(b'<?xml version="1.0" encoding="utf-8"?><Interchange>\n')
+    if outfile:
+        outfile.write(b'<?xml version="1.0" encoding="utf-8"?><Interchange>\n')
     seq = 0
     for event, elem in ET.iterparse(infile):
         if elem.tag != 'Object':
             continue
         seq += 1
         num = elem.find('./ObjectIdentity/Number').text
-        num = normalize_id(num, _args.mdacode)
-        if num in objdict:
-            print(f'seq {seq}, ID {num} is a duplicate, ignored.')
+        numn = normalize_id(num, _args.mdacode)
+        if _args.verbose > 1:
+            print(f'{seq:4}. {num}')
+        if numn in objdict:
+            print(f'**** seq {seq}, ID {num} is a duplicate, ignored.')
             continue
-        objdict[num] = ET.tostring(elem, encoding='utf-8').strip()
-    for num in sorted(objdict):
-        outfile.write(objdict[num])
+        objdict[numn] = ET.tostring(elem, encoding='utf-8').strip()
+    if not outfile:
+        return
+    for numn in sorted(objdict):
+        outfile.write(objdict[numn])
         outfile.write(b'\n')
     outfile.write(b'</Interchange>')
 
@@ -38,7 +45,7 @@ def getargs():
         ''')
     parser.add_argument('infile', help='''
         The XML file saved from Modes.''')
-    parser.add_argument('outfile', help='''
+    parser.add_argument('-o', '--outfile', help='''
         The sorted XML file.''')
     parser.add_argument('--encoding', default='utf-8', help='''
         Set the input encoding. Default is utf-8. Output is always utf-8.
@@ -46,6 +53,9 @@ def getargs():
     parser.add_argument('-m', '--mdacode', default=DEFAULT_MDA_CODE, help=f'''
         Specify the MDA code, used in normalizing the accession number.
         The default is "{DEFAULT_MDA_CODE}".
+        ''')
+    parser.add_argument('-v', '--verbose', type=int, default=1, help='''
+        Set the verbosity. The default is 1 which prints summary information.
         ''')
     args = parser.parse_args()
     return args
@@ -55,5 +65,5 @@ if __name__ == '__main__':
     assert sys.version_info >= (3, 7)
     _args = getargs()
     infile = open(_args.infile, encoding=_args.encoding)
-    outfile = open(_args.outfile, 'wb')
+    outfile = open(_args.outfile, 'wb') if _args.outfile else None
     main()
