12fa83d1bd400397ae268cc3c36c60a7d63e3245
Mike Gleen <mike.gleen@gmail.com>
2022-11-03 10:09:17 +0000

    adopt

---
+++ bin/update/2022-11-01_adopt.sh
@@ -0,0 +1,2 @@
+python src/update_from_csv.py prod_update/normal/2022-10-31_loc_JB010.xml  prod_update/normal/2022-11-01_adopt.xml -c src/cfg/y010_adopt_a_picture.yml -m results/csv/sally/pictures_adopted.csv  --serial 'Accn. No.' -a
+
