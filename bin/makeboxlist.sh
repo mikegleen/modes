3a9bb40683482e37b86da710eacf490e665acfc7
Mike Gleen <mike.gleen@gmail.com>
2023-08-17 10:08:37 +0100

    minor

--- bin/makeboxlist.sh
+++
@@ -1,9 +0,0 @@
-# . activate py6
-set -e
-pushd ~/pyprj/hrm/modes
-python src/list_by_box.py results/jb/xml/f_object_8.xml tmp/jblistbox.csv
-python src/list_by_box.py results/sh/xml/sh-06.xml tmp/shlistbox.csv
-cat tmp/jblistbox.csv tmp/shlistbox.csv >tmp/bothlistbox.csv
-sort -t, -k1,2 tmp/bothlistbox.csv >tmp/sortedlistbox.csv
-cut -d , -f 2- tmp/sortedlistbox.csv >tmp/listbox.csv
-bin/putbom.sh tmp/listbox.csv tmp/bomlistbox.csv
---
+++ doc/source/count_values.rst
@@ -0,0 +1,8 @@
+list_imgs
+=========
+
+.. argparse::
+   :filename: ../src/web/list_imgs.py
+   :func: getparser
+   :prog: list_imgs.py
+
