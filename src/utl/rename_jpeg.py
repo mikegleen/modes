2ffe9c9f9c32f63e6048bcb14d2a66b4430b0a08
Mike Gleen <mike.gleen@gmail.com>
2023-09-27 17:48:30 +0100

    refactor

--- src/utl/rename_jpeg.py
+++ src/utl/rename_jpeg.py
@@ -3,15 +3,22 @@
 """
 import os.path
 import sys
-indir = sys.argv[1]
 
-files = os.listdir(indir)
 
-for fn in files:
-    prefix, ext = os.path.splitext(fn)
-    if ext.lower() == '.jpeg':
-        ext = '.jpg'
-        src = os.path.join(indir, fn)
-        dst = os.path.join(indir, prefix + ext)
-        print(f'\n{src} -> {dst}')
-        os.rename(src, dst)
+def main():
+    indir = sys.argv[1]
+
+    files = os.listdir(indir)
+
+    for fn in files:
+        prefix, ext = os.path.splitext(fn)
+        if ext.lower() == '.jpeg':
+            ext = '.jpg'
+            src = os.path.join(indir, fn)
+            dst = os.path.join(indir, prefix + ext)
+            print(f'\n{src} -> {dst}')
+            os.rename(src, dst)
+
+
+if __name__ == '__main__':
+    main()
