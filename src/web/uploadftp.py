28a61e1ded1497f65846f34f8be44866fd3c9135
Mike Gleen <mike.gleen@gmail.com>
2023-10-18 09:47:19 +0100

    refactor.

--- src/web/uploadftp.py
+++ src/web/uploadftp.py
@@ -24,6 +24,7 @@ PASSWORDFILE = 'etc/passwd'
 COLLECTION_PREFIX = 'collection_'
 VERBOSE = 2
 DRYRUN = False
+IMGFILES = ('.jpg', '.jpeg', '.png')
 
 
 def trace(level, template, *args, color=None):
@@ -34,46 +35,49 @@ def trace(level, template, *args, color=None):
             print(template.format(*args))
 
 
-if len(sys.argv) < 2:
-    trace(0, 'One parameter required, the directory holding images to upload. Exiting.', color=Fore.RED)
-    sys.exit(1)
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        trace(0, 'One parameter required, the directory holding '
+                 'images to upload. Exiting.', color=Fore.RED)
+        sys.exit(1)
 
-sending_dir = sys.argv[1]
-files = os.listdir(sending_dir)
-with open(PASSWORDFILE) as pwfile:
-    password = pwfile.read().strip()
-os.chdir(sending_dir)
-session = FTP(HOST, USER, password)
+    sending_dir = sys.argv[1]
+    files = os.listdir(sending_dir)
+    with open(PASSWORDFILE) as pwfile:
+        password = pwfile.read().strip()
+    os.chdir(sending_dir)
+    session = FTP(HOST, USER, password)
 
-nfiles = len(files)
-trace(1, '{} files to send.', nfiles)
-nsent = 0
-t1 = time.perf_counter()
-for filename in files:
-    if filename.startswith('.'):
-        trace(1, 'Skipping hidden file {}', filename, color=Fore.YELLOW)
-        continue
-    if not filename.endswith('.jpg'):
-        trace(1, 'Skipping non-jpg {}', filename, color=Fore.YELLOW)
-        continue
-    if not filename.startswith(COLLECTION_PREFIX):
+    nfiles = len(files)
+    trace(1, '{} files to send.', nfiles)
+    nsent = 0
+    t1 = time.perf_counter()
+    for filename in files:
+        if filename.startswith('.'):
+            trace(1, 'Skipping hidden file {}', filename, color=Fore.YELLOW)
+            continue
         prefix, suffix = os.path.splitext(filename)
-        prefix = prefix.upper()
-        newfilename = COLLECTION_PREFIX + prefix + suffix
-        os.rename(filename, newfilename)
-        filename = newfilename
-    if DRYRUN:
-        print(f"Dry Run: {filename=}")
-        continue
-    file = open(filename, 'rb')
-    response = session.storbinary(f'STOR {filename}', file)
-    if not response.startswith('226'):
-        trace(1, 'Error in {}: {}', filename, response, color=Fore.RED)
-    else:
-        trace(2, '{}: {}', filename, response)
-    file.close()
-    nsent += 1
-    if nsent % 10 == 0:
-        trace(1, '{} of {} sent', nsent, nfiles)
-elapsed = time.perf_counter() - t1
-trace(1, f'{elapsed:.3f} seconds to send {nsent} file{"" if nsent == 1 else "s"}', color=Fore.GREEN)
+        if suffix.lower() not in IMGFILES:
+            if filename != '.DS_Store':
+                trace(1, 'Skipping non-jpg {}', filename, color=Fore.YELLOW)
+            continue
+        if not filename.startswith(COLLECTION_PREFIX):
+            prefix = prefix.upper()
+            newfilename = COLLECTION_PREFIX + prefix + suffix
+            os.rename(filename, newfilename)
+            filename = newfilename
+        if DRYRUN:
+            print(f"Dry Run: {filename=}")
+            continue
+        file = open(filename, 'rb')
+        response = session.storbinary(f'STOR {filename}', file)
+        if not response.startswith('226'):
+            trace(1, 'Error in {}: {}', filename, response, color=Fore.RED)
+        else:
+            trace(2, '{}: {}', filename, response)
+        file.close()
+        nsent += 1
+        if nsent % 10 == 0:
+            trace(1, '{} of {} sent', nsent, nfiles)
+    elapsed = time.perf_counter() - t1
+    trace(1, f'{elapsed:.3f} seconds to send {nsent} file{"" if nsent == 1 else "s"}', color=Fore.GREEN)
