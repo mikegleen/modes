357cca1afae6bd19ea2a33ebc5a32eef41874570
Mike Gleen <mike.gleen@gmail.com>
2023-11-24 16:57:17 +0000

    Update

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -33,6 +33,7 @@ release = '1.0'
 extensions = [
         'sphinxarg.ext',  # for argparse
         'sphinx.ext.autodoc',
+        'sphinx_simplepdf',
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -54,3 +55,4 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
