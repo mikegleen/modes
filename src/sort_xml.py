43393e5d90820cb3c88fdc1845e491119d6d3240
Mike Gleen <mike.gleen@gmail.com>
2023-12-04 16:27:57 +0100

    Implement --renumber

--- src/sort_xml.py
+++ src/sort_xml.py
@@ -7,12 +7,53 @@ Report and discard any records with duplicate accession numbers.
 """
 
 import argparse
+from inspect import getframeinfo, stack
+import os.path
 import sys
 # noinspection PyPep8Naming
 import xml.etree.ElementTree as ET  # PEP8 doesn't like two uppercase chars
 # import tracemalloc
 
-from utl.normalize import normalize_id, DEFAULT_MDA_CODE
+from utl.normalize import normalize_id, denormalize_id, DEFAULT_MDA_CODE
+from utl.readers import row_dict_reader
+
+from colorama import Fore, Style
+
+
+def trace(level, template, *args, color=None):
+    if _args.verbose >= level:
+        if _args.verbose > 1:
+            caller = getframeinfo(stack()[1][0])
+            print(f'{os.path.basename(caller.filename)} line {caller.lineno}: ', end='')
+        if color:
+            if len(args) == 0:
+                print(f'{color}{template}{Style.RESET_ALL}')
+            else:
+                print(f'{color}{template.format(*args)}{Style.RESET_ALL}')
+        else:
+            if len(args) == 0:
+                print(template)
+            else:
+                print(template.format(*args))
+
+
+def read_renumber():
+    renum_dict = dict()
+    newnum_set = set()
+    for row in row_dict_reader(_args.renumber, verbos=_args.verbose):
+        oldnum = normalize_id(row['OldSerial'], _args.mdacode)
+        newnum = normalize_id(row['NewSerial'], _args.mdacode)
+        if oldnum in renum_dict:
+            trace(0, 'Duplicate OldNumber in CSV: {}', row['OldSerial'],
+                  color=Fore.RED)
+            sys.exit(1)
+        if newnum in newnum_set:
+            trace(0, 'Duplicate NewNumber in CSV: {}', row['NewSerial'],
+                  color=Fore.RED)
+            sys.exit(1)
+        renum_dict[oldnum] = newnum
+        newnum_set.add(newnum)
+    return renum_dict, newnum_set
 
 
 def main():
@@ -29,22 +70,41 @@ def main():
             print(f'{seq:4}. {num}')
         if numn in objdict:
             if _args.nostrict:
-                print(f'**** seq {seq}, ID {num} is a duplicate, ignored.')
+                trace(1, f'**** seq {seq}, ID {num} is a duplicate, '
+                         f'ignored.', color=Fore.LIGHTYELLOW_EX)
                 continue
-            print(f'**** seq {seq}, ID {num} is a duplicate, aborting. Set '
-                  f'--nostrict to allow duplicates which will be discarded.')
+            trace(0, f'**** seq {seq}, ID {num} is a duplicate, aborting. Set '
+                  f'--nostrict to allow duplicates which will be discarded.',
+                  color=Fore.RED)
             sys.exit(1)
-        objdict[numn] = ET.tostring(elem, encoding='utf-8').strip()
+        objdict[numn] = elem
     if not outfile:
         return
+    dupfound = False
+    for newnum in newnumset:
+        if newnum in objdict:
+            dupfound = True
+            trace(1, 'Error: New number {} already exists.',
+                  denormalize_id(newnum), color=Fore.RED)
+    if dupfound:
+        sys.exit(1)
+    for oldnum, newnum in renumdict.items():
+        elem = objdict[oldnum]
+        numelem = elem.find('./ObjectIdentity/Number')
+        numelem.text = denormalize_id(newnum)
+        repro = elem.find('.Reproduction/Filename')
+        if repro is not None:
+            repro.text = f'{denormalize_id(newnum)}.jpg'
+        del objdict[oldnum]
+        objdict[newnum] = elem
     for numn in sorted(objdict):
-        outfile.write(objdict[numn])
-        outfile.write(b'\n')
+        outfile.write(ET.tostring(objdict[numn], encoding='utf-8').strip())
+    outfile.write(b'\n')
     outfile.write(b'</Interchange>')
     return len(objdict)
 
 
-def getargs():
+def getparser():
     parser = argparse.ArgumentParser(description='''
         Create an output file sorted by Object ID.
         ''')
@@ -62,10 +122,20 @@ def getargs():
     parser.add_argument('-n', '--nostrict', action='store_true', help='''
         Set the verbosity. The default is 1 which prints summary information.
         ''')
+    parser.add_argument('-r', '--renumber', help=f'''
+        A CSV file containing two columns with headings OldSerial and NewSerial.
+        The access numbers will be changed accordingly. The program will abort
+        if you try to  create a duplicate accession number.
+        ''')
     parser.add_argument('-v', '--verbose', type=int, default=1, help='''
         Set the verbosity. The default is 1 which prints summary information.
         ''')
-    args = parser.parse_args()
+    return parser
+
+
+def getargs(argv):
+    parser = getparser()
+    args = parser.parse_args(args=argv[1:])
     return args
 
 
@@ -73,12 +143,14 @@ if __name__ == '__main__':
     assert sys.version_info >= (3, 7)
     if len(sys.argv) == 1:
         sys.argv.append('-h')
-    _args = getargs()
-    print('Begin sort_xml.')
+    _args = getargs(sys.argv)
+    trace(1, 'Begin sort_xml.', color=Fore.GREEN)
     infile = open(_args.infile, encoding=_args.encoding)
     outfile = open(_args.outfile, 'wb') if _args.outfile else None
+    renumdict, newnumset = read_renumber()
     # tracemalloc.start()
     numobjs = main()
     # tm = tracemalloc.get_traced_memory()
     # print(f'End sort_xml. {numobjs} objects written. Max memory: {tm[1]:,} bytes.')
-    print(f'End sort_xml. {numobjs} objects written.')
+    trace(1, f'End sort_xml. {numobjs} objects written.',
+          color=Fore.GREEN)
