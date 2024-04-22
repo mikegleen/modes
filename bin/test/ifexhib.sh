ebadc361569b318d54b5a19e24f8c3ed66575941
Mike Gleen <mike.gleen@gmail.com>
2024-04-22 10:33:53 +0100

    minor

--- bin/test/ifexhib.sh
+++ bin/test/ifexhib.sh
@@ -3,8 +3,7 @@ set -e
 INXML=2024-04-07_JB119.xml
 SCRIPT=$(python -c "print('$ZSH_ARGZERO'.split('.')[0].split('/')[-1])")
 cat >tmp/$SCRIPT.yml <<EOF
-cmd: ifanyeq
-xpath: ./Exhibition/Place
-value: Jersey Arts Centre
+cmd: ifexhib
+value: 18
 EOF
 python src/xml2csv.py prod_update/normal/$INXML results/reports/$SCRIPT.csv -c tmp/$SCRIPT.yml
