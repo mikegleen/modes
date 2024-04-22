0f7638175cc9ed99c31cb6d389b61a7d83c9a155
Mike Gleen <mike.gleen@gmail.com>
2024-04-22 10:36:01 +0100

    minor

--- src/once/x042_to_yml.py
+++ src/once/x042_to_yml.py
@@ -1,14 +1,11 @@
 """
 
 """
-import csv
-from datetime import date
 import sys
 # import yaml
 from ruamel.yaml import YAML
 
-from utl.exhibition_list import get_exhibition_dict, get_exhibition_set
-from exhibition import ExhibitionTuple
+from utl.exhibition_list import get_exhibition_dict, get_inverted_exhibition_dict
 
 yaml = YAML(typ='safe')
 
@@ -16,7 +13,7 @@ yaml = YAML(typ='safe')
 if __name__ == '__main__':
     assert sys.version_info >= (3, 9)
     exd = get_exhibition_dict()
-    exs = get_exhibition_set(exd)
+    exs = get_inverted_exhibition_dict()
     # print(exd)
     print(f'{type(exs)=}')
     exs = set([tuple(x) for x in exs])
