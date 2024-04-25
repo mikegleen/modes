1365b59ad92a721ad4770d6eda6a915ac57b3504
Mike Gleen <mike.gleen@gmail.com>
2024-04-25 17:28:07 +0100

    minor

--- bin/test/ifexhib.sh
+++ bin/test/ifexhib.sh
@@ -4,6 +4,17 @@ INXML=2024-04-07_JB119.xml
 SCRIPT=$(python -c "print('$ZSH_ARGZERO'.split('.')[0].split('/')[-1])")
 cat >tmp/$SCRIPT.yml <<EOF
 cmd: ifexhib
-value: 18
+value: 34
+---
+cmd: column
+xpath: ./ObjectLocation[@elementtype="normal location"]/Location
+title: Normal Loc
+---
+cmd: column
+xpath: ./Identification/Title
+---
+# cmd: column
+# xpath: ./Identification/BriefDescription
+# ---
 EOF
-python src/xml2csv.py prod_update/normal/$INXML results/reports/$SCRIPT.csv -c tmp/$SCRIPT.yml
+python src/xml2csv.py prod_update/normal/$INXML results/reports/$SCRIPT.csv -c tmp/$SCRIPT.yml --heading -b
