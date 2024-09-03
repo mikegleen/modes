d8a99f8a72db16184f594bcc2aa42867c7707540
Mike Gleen <mike.gleen@gmail.com>
2024-09-03 16:48:44 +0100

    minor

--- src/once/x055_part.py
+++ src/once/x055_part.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-    Modify the XML structure, undoing some of the work of add_note.
+    Modify the XML structure.
 
-    Undo this change: Change the <Part> child of <Measurement> to be an attribute.
+    Rationalize the ``Description/Measurement/Part`` field.
 
+    For cutting, ephemera, and letter elementtypes, the ``Description`` element
+    group must have one ``Measurement/Part`` element with a text value of ``Image``.
 
 """
 import argparse
@@ -17,16 +19,42 @@ def trace(level, template, *args):
         print(template.format(*args))
 
 
-def one_elt(elt):
-    des = elt.find('./Description')
-    ET.SubElement(des, 'Note')
+def one_elt(elt, objnum) -> bool:
+    rtn = False
+    etype = elt.get('elementtype')
+    if etype is None:
+        print(objnum, 'no elementtype')
+        return rtn
+    # print(objnum)
+    des = elt.find('Description')
+    if des is None:
+        return rtn
+    aspects = des.findall('Aspect')
+    for aspect in aspects:
+        keys = aspect.findall('Keyword')
+        if len(keys) != 2:
+            continue
+        if keys[0].text == 'pages':
+            keys[1].tag = 'Reading'
+            rtn = True
+
     measlist = des.findall('Measurement')
-    for meas in measlist:
-        part = meas.get('part')
-        del meas.attrib['part']
-        newpart = ET.Element('Part')
-        newpart.text = part
-        meas.insert(0, newpart)
+    if len(measlist) == 3:
+        return rtn
+    if etype == 'ephemera':
+        meas = measlist[0]
+        part = meas.find('Part')
+        if part is None:
+            part = ET.Element('Part')
+            meas.insert(0, part)
+        part.text = 'Image'
+        rtn = True
+    elif etype == 'cutting':
+        meas = measlist[0]
+        part = meas.find('Part')
+        part.text = 'Image'
+        rtn = True
+    return rtn
 
 
 def one_object(oldobj):
@@ -35,16 +63,18 @@ def one_object(oldobj):
     :param oldobj: the Object from the old file
     :return: None. The updated object is written to the output XML file.
     """
-    global object_number
+    global object_number, nwritten
     numelt = oldobj.find('./ObjectIdentity/Number')
     if numelt is not None:
         object_number = numelt.text
     else:
         object_number = 'Missing'
     trace(2, '**** {}', object_number)
-    one_elt(oldobj)
+    rtn = one_elt(oldobj, object_number)
     encoding = 'us-ascii' if _args.ascii else 'utf-8'
-    outfile.write(ET.tostring(oldobj, encoding=encoding))
+    if rtn or _args.all:
+        nwritten += 1
+        outfile.write(ET.tostring(oldobj, encoding=encoding))
 
 
 def main():
@@ -67,7 +97,9 @@ def getargs():
         The input XML file''')
     parser.add_argument('outfile', help='''
         The output XML file.''')
-    parser.add_argument('-a', '--ascii', action='store_true', help='''
+    parser.add_argument('-a', '--all', action='store_true', help='''
+        Write all records. ''')
+    parser.add_argument('--ascii', action='store_true', help='''
         Create the output XML file using the us-ascii encoding rather than
         utf-8. This means that non-ascii characters will be encoded with
         sequences such as "&#8220" meaning the left double quote character.
@@ -85,8 +117,10 @@ if __name__ == '__main__':
     if sys.version_info.major < 3:
         raise ImportError('requires Python 3')
     object_number = ''
+    nwritten = 0
     _args = getargs()
     infile = open(_args.infile)
     outfile = open(_args.outfile, 'wb')
     main()
+    print(f'{nwritten} objects written.')
 
