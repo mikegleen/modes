40e2e56656c9c21d87757a44fc75475d52b47cc3
Mike Gleen <mike.gleen@gmail.com>
2025-02-22 17:19:11 +0000

    Add Steadman exhibition.

--- src/utl/exhibition_list.py
+++ src/utl/exhibition_list.py
@@ -54,6 +54,7 @@ EXSTR = '''Serial,DateBegin,DateEnd,ExhibitionName,Place
 36,1992-02-15,1992-03-20,"The Brothers Robinson:Charles, Thomas Heath and William Heath Robinson",Chris Beatles Gallery
 37,2024-01-13,2024-03-24,"Mary V. Wheelhouse: suffragette, illustrator and toymaker"
 38,2016-10-01,2017-04-17,Den Store Opfindelses Udstilling,Storm P. Museum
+39,2025-02-15,2025-05-10,Ralph Steadman: INKling
 '''
 # data for testing
 '''
