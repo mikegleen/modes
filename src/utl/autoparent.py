123a42a1631f0a01b56e65cc7e7d63b991cd4fc6
Mike Gleen <mike.gleen@gmail.com>
2025-03-27 09:18:39 +0000

    Handle attribute a child.

--- src/utl/autoparent.py
+++ src/utl/autoparent.py
@@ -5,34 +5,87 @@
 
     * a simple tag - If the element exists, it will be returned. If not, the parent will
       be examined and if it exists the a new element will be created and returned.
+    * tag[child] - If necessary, a child will be created
     * tag[child="value"] - If necessary, a child will be created
     * tag[@attribute="value"] - If necessary, an attribute will be created.
+    * tag[@attribute] - If necessary, an attribute will be created.
 """
+import re
 import sys
 # noinspection PyPep8Naming
 import xml.etree.ElementTree as ET
 
 
-def getelt(root: ET.Element, xpath: str) -> ET.Element:
+patstr = r"""(?P<tag>\w*)
+             (?P<qual>\[              # @ section enclosed by "[]"
+             (?P<at>@?)               # indicating that the ref is an attribute
+             ((?P<ref>\w+)           # either a child name or attribute
+             (="(?P<val>[^"]+)")?)?])?  # the attribute or child text value
+          """
+
+pat = re.compile(patstr, flags=re.VERBOSE)
+
+
+def getelt(root: ET.Element, xpath: str, maxcreate=0) -> ET.Element | None:
     """
 
     :param root:
     :param xpath:
-    :return: the element whose path is described
+    :param maxcreate: The number of elements to create. If set to 1,
+    only the final element will be created. If set to 2, the parent will also be created.
+    If set to zero, the element must exist.
+    :return: the element whose path is described or None if the xpath doesn't exist
     """
-
     elt = root.find(xpath)
     if elt is not None:
         return elt
+    if maxcreate <= 0:
+        return None
     epath = xpath
     elts = epath.split('/')
     # get the parent
-    parentpath = getelt(root, '/'.join(elts[:-1]))
-    elt = ET.SubElement(parentpath, elts[-1])
+    parentpath = getelt(root, '/'.join(elts[:-1]), maxcreate-1)
+    newelt = elts[-1]
+    m = pat.match(newelt)
+    newtag = m['tag']
+    elt = ET.SubElement(parentpath, newtag)
+    if m['qual']:
+        atsign = m['at']
+        ref = m['ref']
+        val = m['val']
+        if atsign:
+            elt.set(ref, val)
+        else:
+            child = ET.SubElement(elt, ref)
+            child.text = val
+
     return elt
 
 
+def test1pat(s: str):
+    m = pat.match(s)
+    print(f'\n{s=}')
+    if m:
+        print('    tag=', m['tag'])
+        print(f'    {m["qual"]=}')
+        print('    at=', m['at'])
+        print('    ref=', m['ref'])
+        print('    val=', m['val'])
+    else:
+        print('failed', s)
+
+
+def testpat():
+    test1pat('Number')
+    test1pat('ObjectLocation[@elementtype]')
+    test1pat('ObjectLocation[@elementtype="normal location"]')
+    test1pat('Measurement[Part]')
+
+
 if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        testpat()
+        sys.exit()
     from utl import readers
     ip1 = readers.object_reader(sys.argv[1])
     outfile = open(sys.argv[2], 'wb')
