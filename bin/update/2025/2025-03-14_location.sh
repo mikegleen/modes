695901afeb16d8aad4c8e6a2778eb159c5f86913
Mike Gleen <mike.gleen@gmail.com>
2025-04-09 22:20:44 +0100

    Add comment for manual change applied to move Note in Date.

--- bin/update/2025/2025-03-14_location.sh
+++ bin/update/2025/2025-03-14_location.sh
@@ -7,6 +7,26 @@ INXML=2025-03-08_stocktake_condition.xml
 #
 #   Manual changes applied:
 # 2018.19 Move <Note> element outside of Date element group.
+#
+# Change:
+#    <ObjectLocation elementtype="previous location">
+#        <Location>R7</Location>
+#        <Date>
+#            <DateBegin>29.11.2024</DateBegin>
+#            <DateEnd>5.3.2025</DateEnd>
+#            <Note>Stocktake 2024 (Patched)</Note>
+#        </Date>
+#    </ObjectLocation>
+# To:
+#    <ObjectLocation elementtype="previous location">
+#        <Location>R7</Location>
+#        <Date>
+#            <DateBegin>29.11.2024</DateBegin>
+#            <DateEnd>5.3.2025</DateEnd>
+#        </Date>
+#        <Note>Stocktake 2024 (Patched)</Note>
+#    </ObjectLocation>
+#
 #         Add note to normal location.
 #
 INXML=2025-03-08_stocktake_loc2.xml
