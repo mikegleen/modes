15836d6a3f540f2fc0d09105a8929101c89279de
Mike Gleen <mike.gleen@gmail.com>
2025-10-24 17:30:14 +0100

    move files

--- bin/collection/batch005.sh
+++
@@ -1,9 +0,0 @@
-#
-# For the files in a batch, create the CSV file to be uploaded to WordPress for the collection images.
-#
-pushd ~/pyprj/hrm/modes
-export BATCH=batch005
-export REVISION=
-export MODESFILE=prod_update/normal/2022-04-11_set_circa.xml
-#
-bin/collection/batch_sub.sh
