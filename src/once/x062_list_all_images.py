a14fc5614718cfb9020de0327b1ec1453a218b64
Mike Gleen <mike.gleen@gmail.com>
2025-10-24 17:31:28 +0100

    new

---
+++ src/once/x062_list_all_images.py
@@ -0,0 +1,92 @@
+import argparse
+import csv
+import os.path
+import re
+import sys
+
+from web.webutil import COLLECTION_PREFIX
+from utl.normalize import DEFAULT_MDA_CODE
+
+DESCRIPTION = """
+    Examine a tree of folders recursively and print the filename, file size, and path
+    of all images that have accession numbers in the filename.
+    
+    Output is a CSV file with the first column being the filename and subsequent columns
+    being the containing folders.
+"""
+
+IMGFILES = ('.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff')
+
+
+def one_file(parentpath, filename):
+    prefix, suffix = os.path.splitext(filename)
+    if suffix.lower() not in IMGFILES:
+        return
+    rootfn = prefix.removeprefix(COLLECTION_PREFIX)
+    if rootfn.startswith(_args.mdacode) or re.match(r'((JB)|(SH)|L)\d+', rootfn):
+        # print(f'{filename},{os.path.join(parentpath, filename)}')
+        #     return
+        # if re.match(r'^((JB)|(SH)|L)\d+', rootfn):
+        # print(f'{filename},{parentpath}')
+        if filename not in table:
+            table[filename] = [filename]
+        fullpath = os.path.join(parentpath, filename)
+        filesize = os.path.getsize(fullpath)
+
+        # table[filename].append(parentpath)
+        table[filename] += [parentpath, filesize]
+    return
+
+
+def main(indir):
+    for file in os.listdir(indir):
+        filepath = os.path.join(indir, file)
+        if os.path.isdir(filepath):
+            if file == "2023-10-22_high_res_images":
+                continue
+            main(filepath)  # recursively walk subdirectory
+        else:
+            one_file(indir, file)
+
+
+def print_table():
+    maxlen = 0
+    maxfn = ''
+    for fn in sorted(table):
+        if len(table[fn]) > maxlen:
+            maxlen = len(table[fn])
+            maxfn = fn
+        csvwriter.writerow(table[fn])
+    print(f'{maxfn=}, {maxlen=}')
+
+
+def getparser():
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('indir', help='''
+        Folder containing files to search.''')
+    parser.add_argument('outfile', help='''
+        File to contain the output CSV file.''')
+    parser.add_argument('-m', '--mdacode', default=DEFAULT_MDA_CODE, help=f'''
+        Specify the MDA code. The default is {DEFAULT_MDA_CODE}''')
+    parser.add_argument('-v', '--verbose', type=int, default=1, help='''
+        Set the verbosity. The default is 1 which prints summary information.
+        ''')
+    return parser
+
+
+def getargs(argv):
+    parser = getparser()
+    args = parser.parse_args(args=argv[1:])
+    return args
+
+
+if __name__ == '__main__':
+    assert sys.version_info >= (3, 13)
+    if len(sys.argv) == 1:
+        sys.argv.append('-h')
+    _args = getargs(sys.argv)
+    table = dict()
+    csvfile = open(_args.outfile, 'w')
+    csvwriter = csv.writer(csvfile)
+    main(_args.indir)
+    print_table()
---
+++ src/once/x063_merge_image_lists.py
@@ -0,0 +1,83 @@
+import argparse
+import csv
+import os.path
+import re
+import sys
+
+from web.webutil import COLLECTION_PREFIX
+from utl.normalize import DEFAULT_MDA_CODE
+
+DESCRIPTION = """
+    Input is two files created by x061_list_all_images.py.
+    Output is a single file with the rows merged.
+"""
+
+
+def main():
+    for filename in [_args.infile1, _args.infile2]:
+        incsvfile = open(filename)
+        reader = csv.reader(incsvfile)
+        nrow = 0
+        for row in reader:
+            nrow += 1
+            if row[0] not in table:
+                table[row[0]] = row
+            else:
+                table[row[0]] += row[1:]
+        print(f'file {filename}: {nrow} rows')
+    incsvfile.close()
+
+    outcsvfile = open(_args.outfile, 'w')
+    csvwriter = csv.writer(outcsvfile)
+    maxlen = 0
+    maxfn = ''
+    nwritten = 0
+    for fn in sorted(table):
+        row = table[fn]
+        if len(row) > maxlen:
+            maxlen = len(row)
+            maxfn = fn
+        if _args.dups:
+            found = False
+            sizes = set()
+            for i in range(2, len(row), 2):
+                if row[i] in sizes:
+                    found = True
+                sizes.add(row[i])
+            if not found:
+                continue
+        csvwriter.writerow(row)
+        nwritten += 1
+    print(f'{maxfn=}, {maxlen=}, {nwritten=}')
+
+
+def getparser():
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('infile1', help='''
+        file to merge.''')
+    parser.add_argument('infile2', help='''
+        another file to merge.''')
+    parser.add_argument('outfile', help='''
+        File to contain the output CSV file.''')
+    parser.add_argument('-p', '--dups', action='store_true', help='''
+        Only write to output if there are multiple copies of the same file.
+        ''')
+    parser.add_argument('-v', '--verbose', type=int, default=1, help='''
+        Set the verbosity. The default is 1 which prints summary information.
+        ''')
+    return parser
+
+
+def getargs(argv):
+    parser = getparser()
+    args = parser.parse_args(args=argv[1:])
+    return args
+
+
+if __name__ == '__main__':
+    assert sys.version_info >= (3, 13)
+    if len(sys.argv) == 1:
+        sys.argv.append('-h')
+    _args = getargs(sys.argv)
+    table = dict()
+    main()
