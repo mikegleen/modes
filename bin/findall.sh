586c405743034b0056c25abd1cb97948ac6fe555
Mike Gleen <mike.gleen@gmail.com>
2025-11-09 17:18:57 +0000

    minor

--- bin/findall.sh
+++ bin/findall.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
-find ../collection/aawebimgs -name "*${1:u}.jpg" -exec ls -l {} \;
+# find ../collection/aawebimgs -name "*${1:u}.jpg" -exec ls -l {} \;
+# find ../collection/candidates_large -name "*${1:u}.jpg" -exec ls -l {} \;
+find ../collection -name "*${1:u}.jpg" -exec ls -l {} \;
 find /Users/mlg/Pictures/VueScan -name "*${1:u}.jpg" -exec ls -l {} \;
 find ../scans -name "*${1:u}.jpg" -exec ls -l {} \;
 echo ---
