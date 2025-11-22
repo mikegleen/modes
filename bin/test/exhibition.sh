9f96dedcbc54478c305b2534ebffdd0795fa8afc
Mike Gleen <mike.gleen@gmail.com>
2025-11-22 23:23:24 +0000

    deleted

--- bin/test/exhibition.sh
+++
@@ -1,3 +0,0 @@
-python src/exhibition.py test/exhibition/xml/result1.xml test/exhibition/xml/result5.xml -e 999 -m test/exhibition/csv/test1.csv -s 1
-python src/exhibition.py test/exhibition/xml/result5.xml test/exhibition/xml/result6.xml -e 999 -m test/exhibition/csv/test1.csv -s 1 --old_date 2021-10-15
-
