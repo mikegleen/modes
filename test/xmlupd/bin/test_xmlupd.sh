2267574044f33455a1a85fe4830bc77c7a895141
Mike Gleen <mike.gleen@gmail.com>
2025-11-22 23:45:22 +0000

    minor

--- test/xmlupd/bin/test_xmlupd.sh
+++ test/xmlupd/bin/test_xmlupd.sh
@@ -2,6 +2,7 @@
 export tpath=test/xmlupd
 export tfile=newmaster.xml
 #
+rm $tpath/results/$tfile
 python src/xmlupd.py $tpath/xml/master.xml $tpath/xml/detail.xml -o $tpath/results/$tfile -v 0 $*
 #
 source test/bin/validate.sh $? $tfile
