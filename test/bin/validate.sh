2dee95dbc870e1dfc40dc3ffe88770c51714c39f
Mike Gleen <mike.gleen@gmail.com>
2025-11-22 23:13:30 +0000

    init

---
+++ test/bin/validate.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+#
+#   Caller must pass $? from the program under test down as the first parameter.
+#   Second parameter is the name of the result file to compare.
+#
+#
+#   Get the path of the parent of the results and baseline dirctories:
+#   test/exhibition/bin/test_exhibition.sh -> test/exhibition
+tpath=$(dirname $(dirname $ZSH_ARGZERO))
+if [ $1 -ne 0 ]; then
+    printf "${RED}Test aborted.${NC} (${ZSH_ARGZERO:t:r})\n"
+    exit
+fi
+if [ -f $tpath/baseline/$2 ]; then
+    diff -q $tpath/baseline/$2 $tpath/results/$2 >/dev/null
+    if [ $? -eq 1 ]; then
+        printf "${RED}Test failed.${NC} (${ZSH_ARGZERO:t:r})\n"
+    else
+        printf "Test passed. (${ZSH_ARGZERO:t:r})\n"
+    fi
+else
+    cp $tpath/results/$2 $tpath/baseline
+    echo Initializing baseline.
+fi
