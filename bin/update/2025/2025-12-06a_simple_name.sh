5364de10fe868ace39895b474207743cd3f0ef05
Mike Gleen <mike.gleen@gmail.com>
2025-12-08 20:55:09 +0100

    add runtime report

--- bin/update/2025/2025-12-06a_simple_name.sh
+++ bin/update/2025/2025-12-06a_simple_name.sh
@@ -3,6 +3,7 @@
 #   Add simple name elements to all records.
 #
 set -e
+start=`date +%s.%N`
 INXML=prod_update/normal/2025-12-04_brackets.xml
 #
 SCRIPT=${ZSH_ARGZERO:t:r}  # ZSH doc 14.1.4 Modifiers
@@ -15,3 +16,8 @@ python src/xmldiff.py $INXML $OUTXML -o $DELTAXML
 #
 bin/syncupdate.sh
 bin/syncdelta.sh
+end=`date +%s.%N`
+BC_ENV_ARGS="-e scale=3"
+runtime=$( echo "($end - $start)/1.000" | bc )
+echo runtime: $runtime seconds
+
