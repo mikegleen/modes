c952f31d5caef873e1337f9a89b58aa5d6f28d90
Mike Gleen <mike.gleen@gmail.com>
2025-12-08 16:00:40 +0100

    cosmetic

--- src/once/x069_simple_name.py
+++ src/once/x069_simple_name.py
@@ -1,11 +1,12 @@
 """
     Insert <ObjectName elementtype="simple name"> elements into all objects.
 """
+import os
 import time
-
 import sys
 # noinspection PyPep8Naming
 import xml.etree.ElementTree as ET
+
 from utl.readers import object_reader
 
 
@@ -42,13 +43,13 @@ def main(idnum, obj):
 
 if __name__ == '__main__':
     assert sys.version_info >= (3, 13)
+    print(f"Begin {os.path.basename(sys.argv[0])}.")
     t1 = time.perf_counter()
     infile = sys.argv[1]
     outfile = open(sys.argv[2], 'wb')
     outfile.write(b'<?xml version="1.0"?><Interchange>\n')
     for id_num, ob_ject in object_reader(infile):
-        # print(f'{ob_ject=}')
         main(id_num, ob_ject)
     outfile.write(b'</Interchange>')
     elapsed = time.perf_counter() - t1
-    print(f'Elapsed: {elapsed:5.2f} seconds.')
+    print(f'Elapsed: {elapsed:6.3f} seconds.')
