86dacd0f7be4fee89f67b1ce43a20ca6f607928f
Mike Gleen <mike.gleen@gmail.com>
2026-01-14 07:48:28 +0000

    bug fix

--- bin/misc/sort_prod_save.sh
+++ bin/misc/sort_prod_save.sh
@@ -4,5 +4,5 @@ pushd /Users/mlg/pyprj/hrm/modes
 #
 INXML=$(python src/utl/x066_latest.py -i prod_save/normal)
 OUTXML=${INXML: :-4}_sorted.xml
-python src/sort_xml.py $INXML $OUTXML
+python src/sort_xml.py -i $INXML -o $OUTXML
 bin/syncsave.sh
