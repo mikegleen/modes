f8cf116611a97d955f85f53bf1543e690356e6b7
Mike Gleen <mike.gleen@gmail.com>
2026-05-03 09:02:43 +0100

    add test of single file

--- bin/test/find_doc_errors.sh
+++ bin/test/find_doc_errors.sh
@@ -1,7 +1,15 @@
+#!/usr/bin/env bash
 echo begin find errors
 pushd doc
+if [[ -n ${1+x} ]]; then  # true if parameter exists
+    echo testing source/$1.rst
+    touch source/$1.rst
+    make html
+    exit
+fi
+#
 for file in source/*.rst ; do
     touch $file
     echo file: $file
-    make html|grep ERROR
+    make html
 done
