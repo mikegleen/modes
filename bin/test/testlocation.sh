cd1ae03367ac45d3f6caf7ea27292cb9ef3ffe76
Mike Gleen <mike.gleen@gmail.com>
2026-05-15 10:47:28 +0200

    refactor

--- bin/test/testlocation.sh
+++ bin/test/testlocation.sh
@@ -1,5 +1,6 @@
-python src/location.py update data/test/location/xml/location_$1.xml \
-                              tmp/location_$1.xml \
+set -e
+python src/location.py update --infile data/test/location/xml/location_$1.xml \
+                              --outfile tmp/location_$1.xml \
                               -m data/test/location/csv/location_$1.csv \
                               -r "Good Reason" -c
-bin/pretty /Users/mlg/pyprj/hrm/modes/tmp/location_$1.xml
+bin/pretty.sh /Users/mlg/pyprj/hrm/modes/tmp/location_$1.xml
