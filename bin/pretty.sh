8cde75b57c442fe64cc9984beec91c77b793912d
Mike Gleen <mike.gleen@gmail.com>
2026-05-15 10:45:05 +0200

    rename

---
+++ bin/pretty.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+CONDAENV=py314
+set -e
+if [[ "$CONDA_DEFAULT_ENV" != $CONDAENV ]] ; then
+    echo Activating $CONDAENV...
+    eval "$(conda shell.bash hook)"
+    conda activate $CONDAENV
+fi
+pushd ~/pyprj/hrm/modes
+INXML=`basename $1`
+echo INXML = $INXML
+INDIR=`dirname $1`
+echo INDIR = $INDIR
+BASE=`basename $INDIR`
+if [[ "$BASE" != "normal" ]]; then
+    echo XML file must be in folder named "normal". Exiting
+    exit 1
+fi
+OUTDIR=`dirname $INDIR`/pretty
+echo OUTDIR = $OUTDIR
+BASENAME=`python3 -c "print('$INXML'.split('.')[0])"`
+PRETTYPATH="$OUTDIR/${BASENAME}_pretty.xml"
+echo Creating: $PRETTYPATH
+python src/normalize_xml.py -p -n $1 $PRETTYPATH
