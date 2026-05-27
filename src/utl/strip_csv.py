a07f3a70a9bee72cc7501128f79f6f1a716d9372
Mike Gleen <mike.gleen@gmail.com>
2026-05-27 15:54:45 +0100

    remove deprecated codecs.open call.

--- src/utl/strip_csv.py
+++ src/utl/strip_csv.py
@@ -1,14 +1,13 @@
 """
     For each cell in a CSV file, strip leading and trailing whitespace.
 """
-import codecs
 import csv
 import sys
 
 
 def main():
-    incsv = codecs.open(sys.argv[1], 'r', 'utf-8-sig')
-    outcsv = codecs.open(sys.argv[2], 'w', 'utf-8-sig')
+    incsv = open(sys.argv[1],  encoding='utf-8-sig')
+    outcsv = open(sys.argv[2], 'w', encoding='utf-8-sig')
     outwriter = csv.writer(outcsv)
     for row in csv.reader(incsv):
         for column in range(len(row)):
