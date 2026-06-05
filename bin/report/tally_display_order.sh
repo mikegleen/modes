971f5dd2b057b1062318f3ddfd7698574a72edbe
Mike Gleen <mike.gleen@gmail.com>
2026-06-05 12:31:51 +0100

    update

--- bin/report/tally_display_order.sh
+++ bin/report/tally_display_order.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 set -e
 INXML=$(python src/utl/x066_latest.py -i prod_save/normal)
-SCRIPT=$(python -c "print('$ZSH_ARGZERO'.split('.')[0].split('/')[-1])")
+SCRIPT="$(basename -- "${0%.*}")"
 # echo $SCRIPT
 cat >tmp/$SCRIPT.yml <<EOF
 column: display order
 xpath: ./Description/Aspect[Keyword="display order"]/Reading
 EOF
 python src/utl/python_version.py 3.14
-python src/tally_elts.py $INXML -c tmp/$SCRIPT.yml -v 2
+python src/tally_elts.py $INXML -c tmp/$SCRIPT.yml -v 1
