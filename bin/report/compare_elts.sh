986eeb9e5e87f32fdd13ff13e4facd9a48c9c807
Mike Gleen <mike.gleen@gmail.com>
2026-06-05 12:42:13 +0100

    init

---
+++ bin/report/compare_elts.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+set -e
+INXML=$(python src/utl/x066_latest.py -i prod_save/normal)
+SCRIPT="$(basename -- "${0%.*}")"
+# echo $SCRIPT
+cat >tmp/$SCRIPT.yml <<EOF
+cmd: column
+xpath: ./Identification/Title
+---
+cmd: column
+xpath: ./Identification/BriefDescription
+EOF
+python src/compare_elts.py $INXML -c tmp/$SCRIPT.yml -v 1
