09ec08585c9b5135e4081eb1d0ec45586a7b9580
Mike Gleen <mike.gleen@gmail.com>
2026-09-01 08:14:48 +0100

    minor

--- bin/sort_prod_save.sh
+++ bin/sort_prod_save.sh
@@ -3,7 +3,9 @@ set -e
 pushd /Users/mlg/pyprj/hrm/modes
 #
 INXML=$(python src/utl/x066_latest.py -i prod_save/normal)
+echo INXML=$INXML
 INXMLM=$(python src/utl/x066_latest.py -i prod_save/normal --modify s)
+echo INXMLM=$INXMLM
 OUTXML=${INXMLM: :-4}_sorted.xml
 python src/sort_xml.py -i $INXML -o $OUTXML
 rm $INXML
