d28acd6a6288dfa41f3d29a2ef388d75a0363f93
Mike Gleen <mike.gleen@gmail.com>
2026-09-20 13:55:31 +0100

    Only look at relevant files.

--- bin/sort_prod_save.sh
+++ bin/sort_prod_save.sh
@@ -2,11 +2,15 @@
 set -e
 pushd /Users/mlg/pyprj/hrm/modes
 #
-INXML=$(python src/utl/x066_latest.py -i prod_save/normal --skip_date_suffix s)
+# Only look at files that end with "prod_save.xml" and also don't have a date suffix of "s".
+INXML=$(python src/utl/x066_latest.py -i prod_save/normal --skip_date_suffix s --re ".*prod_save\\.xml")
 echo INXML=$INXML
-INXMLM=$(python src/utl/x066_latest.py -i prod_save/normal --modify s --skip_date_suffix s)
+INXMLM=$(python src/utl/x066_latest.py -i prod_save/normal --modify s --skip_date_suffix s --re ".*prod_save\\.xml")
 echo INXMLM=$INXMLM
-OUTXML=${INXMLM: :-4}_sorted.xml
+#
+# Strip off the trailing "save.xml" and append "sorted.xml"
+OUTXML=${INXMLM: :-9}_sorted.xml
+echo OUTXML=$OUTXML
 python src/sort_xml.py -i $INXML -o $OUTXML
 rm $INXML
 bin/syncsave.sh
